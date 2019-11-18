a7f7888bf8183aed424216766a9fa021863c426a
chtseng <chtseng@itri.org.tw>
2019-11-18 09:33:50 +0800

    Exclude drivenet from clang build temporarily.

--- src/scripts/ci/module_build_clang.sh
+++ src/scripts/ci/module_build_clang.sh
@@ -21,7 +21,7 @@ done
 if [[ -d src/sensing/itri_openroadnet/libs_opn/TensorFlow/Installed ]]; then
   rm -rf src/sensing/itri_openroadnet/libs_opn/TensorFlow/Installed
 fi
-blacklist="ndt_gpu;convex_fusion;lidar;output_results_by_dbscan;lidar_squseg_inference;ouster_driver;velodyne_laserscan;velodyne;velodyne_msgs;velodyne_driver;velodyne_pointcloud;lidars_grabber;libs;lidars_preprocessing;dl_data"
+blacklist="ndt_gpu;convex_fusion;lidar;output_results_by_dbscan;lidar_squseg_inference;ouster_driver;velodyne_laserscan;velodyne;velodyne_msgs;velodyne_driver;velodyne_pointcloud;lidars_grabber;libs;lidars_preprocessing;dl_data;localization;drivenet;drivenet_lib"
 
 catkin_make \
     -DCMAKE_BUILD_TYPE=${build_type} \
