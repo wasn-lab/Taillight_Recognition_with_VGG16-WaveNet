42282b0397ae77022037586023295a591982143a
chtseng <chtseng@itri.org.tw>
2019-11-20 11:18:22 +0800

    Move clang build into a step.

--- src/scripts/ci/mr_pipeline.sh
+++ src/scripts/ci/mr_pipeline.sh
@@ -29,10 +29,8 @@ readonly clean_build_status=$(python src/scripts/ci/decide_dirty_clean_build.py)
 echo ${clean_build_status}
 if [[ "${clean_build_status}" =~ "Clean build" ]]; then
   bash src/scripts/ci/module_build.sh
-  bash src/scripts/ci/module_build_clang.sh
 else
   catkin_make
-  catkin_make --build build_clang -DCATKIN_DEVEL_PREFIX=devel_clang
 fi
 
 popd
