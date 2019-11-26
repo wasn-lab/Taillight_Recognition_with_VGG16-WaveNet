072e0b6f9bd48b49452cb53d81c3de0ed71182b6
yiling <amtommy6@gmail.com>
2019-11-26 14:52:26 +0800

    Using cv::empty to replace data is null or rows and cols < 0

--- src/sensing/itri_drivenet/drivenet/src/exception_handling/image_exception_handling.cpp
+++ src/sensing/itri_drivenet/drivenet/src/exception_handling/image_exception_handling.cpp
@@ -2,14 +2,9 @@
 
 bool CheckMatDataValid(Mat src)
 {
-    if (!src.data)
+    if (src.empty())
     {
         std::cout << "Unable to read image." << std::endl;
-        return false;
-    }
-    else if (src.cols <= 0 || src.rows <= 0)
-    {
-        std::cout << "Image cols: " << src.cols << ", rows: " << src.rows << std::endl;
         return false;
     }
     return true;
