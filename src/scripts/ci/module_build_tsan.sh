64acce93f0655b1a2c6b043bc61bc0d01876f121
chtseng <chtseng@itri.org.tw>
2019-12-09 09:48:41 +0800

    Add module_build_tsan.sh.
    SCM-32 Add ASan/TSan bot builders

---
+++ src/scripts/ci/module_build_tsan.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Build executables with address sanitizer (ASAN)
+set -x
+set -e
+
+readonly build_type="${build_type:-Debug}"
+readonly repo_dir=$(git rev-parse --show-toplevel)
+readonly build_dir=build
+readonly devel_dir=devel
+pushd $repo_dir
+
+# clean up the previous build.
+for _dir in ${build_dir} ${devel_dir}; do
+    if [[ -d $_dir ]]; then
+        rm -rf $_dir
+    fi
+done
+# workaround for openroadnet
+if [[ -d src/sensing/itri_openroadnet/libs_opn/TensorFlow/Installed ]]; then
+  rm -rf src/sensing/itri_openroadnet/libs_opn/TensorFlow/Installed
+fi
+blacklist="ndt_gpu;convex_fusion;lidar;output_results_by_dbscan;lidar_squseg_inference;ouster_driver;velodyne_laserscan;velodyne;velodyne_msgs;velodyne_driver;velodyne_pointcloud;lidars_grabber;libs;lidars_preprocessing;localization"
+
+catkin_make \
+    --build ${build_dir} \
+    -DCATKIN_DEVEL_PREFIX=${devel_dir} \
+    -DCMAKE_BUILD_TYPE=${build_type} \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+    -DENABLE_THREAD_SANITIZER=1 \
+    -DCATKIN_BLACKLIST_PACKAGES="$blacklist"
+popd
+
