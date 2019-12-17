2af62bc5cee3efad4e2b1ef3c605c25fd8c74acd
BensonHuang <BensonHuang@itri.org.tw>
2019-12-17 11:49:14 +0800

    Change the original "copy-backup" to queued "move-backup" method for increasing efficiency

--- src/utilities/msg_recorder/scripts/msg_recorder_copy_method.py
+++ src/utilities/msg_recorder/scripts/msg_recorder_copy_method.py
@@ -38,11 +38,11 @@ def erase_last_lines(n=1, erase=False):
             sys.stdout.write(ERASE_LINE)
 #---------------------------------------------------#
 
-class MOVE_QUEUE:
+class COPY_QUEUE:
     """
     This is the class for handling the file copying.
     """
-    def __init__(self, src_dir, dst_dir, num_move_thread=10):
+    def __init__(self, src_dir, dst_dir, num_copy_thread=3):
         """
         This class is dedicated on doing the following command in an efficient way.
             --> shutil.copy2( (self.src_dir + file_name), self.dst_dir)
@@ -54,12 +54,12 @@ class MOVE_QUEUE:
         self.dst_dir = dst_dir
         #
         self.file_Q = Queue.Queue()
-        self.moved_file_list = list()
+        self.copied_file_list = list()
         #
-        self.num_move_thread = num_move_thread
-        self._move_thread_list = list()
+        self.num_copy_thread = num_copy_thread
+        self._copy_thread_list = list()
         # Start the polling thread
-        self._polling_thread = threading.Thread(target=self._move_file_listener)
+        self._polling_thread = threading.Thread(target=self._copy_file_listener)
         self._polling_thread.daemon = True # Use daemon to prevent eternal looping
         self._polling_thread.start()
 
@@ -70,14 +70,13 @@ class MOVE_QUEUE:
         """
         self.file_Q.put(file_name)
 
-    def _move_file_worker(self, file_name):
+    def _copy_file_worker(self, file_name):
         """
-        This is the worker for moving file (blocking function)
+        This is the worker for copying file (blocking function)
         """
-        print("[MoveQ] Moving <%s>." % file_name)
-        # shutil.copy2( (self.src_dir + file_name), self.dst_dir)
-        shutil.move( (self.src_dir + file_name), self.dst_dir)
-        print("[MoveQ] Finishing moving <%s>." % file_name)
+        print("[copyQ] Copying <%s>." % file_name)
+        shutil.copy2( (self.src_dir + file_name), self.dst_dir)
+        print("[copyQ] Finishing copying <%s>." % file_name)
 
     def _remove_idle_threads(self):
         """
@@ -85,17 +84,17 @@ class MOVE_QUEUE:
         """
         #--------------------------------#
         _idx = 0
-        while _idx < len(self._move_thread_list):
-            if not self._move_thread_list[_idx].isAlive():
-                del self._move_thread_list[_idx]
+        while _idx < len(self._copy_thread_list):
+            if not self._copy_thread_list[_idx].isAlive():
+                del self._copy_thread_list[_idx]
                 # _idx = 0 # Re-start from beginning...
                 # NOTE: the _idx is automatically pointing to the next one
             else:
                 _idx += 1
-        # print("[MoveQ] Number of thread busying = %d" % len(self._move_thread_list) )
+        # print("[CopyQ] Number of thread busying = %d" % len(self._copy_thread_list) )
         #--------------------------------#
 
-    def _move_file_listener(self):
+    def _copy_file_listener(self):
         """
         This is the thread worker function for listening the file names from queue.
         """
@@ -103,29 +102,29 @@ class MOVE_QUEUE:
             # Note: this thread will only closed if this program is stopped
             while not self.file_Q.empty():
                 self._remove_idle_threads()
-                if len(self._move_thread_list) >= self.num_move_thread:
+                if len(self._copy_thread_list) >= self.num_copy_thread:
                     # The pool is full, keep waiting
-                    # print("[MoveQ] Copy thread pool is full, keep waiting.")
+                    # print("[CopyQ] Copy thread pool is full, keep waiting.")
                     break
                 a_file = self.file_Q.get()
-                print("[MoveQ] Get <%s> from list." % a_file)
-                if not a_file in self.moved_file_list:
+                print("[copyQ] Get <%s> from list." % a_file)
+                if not a_file in self.copied_file_list:
                     # The file has not been processed
-                    self.moved_file_list.append(a_file)
+                    self.copied_file_list.append(a_file)
                     # Really copy a file (blocked until finished)
-                    # print("[MoveQ] Copying <%s>." % a_file)
+                    # print("[copyQ] Copying <%s>." % a_file)
                     # shutil.copy2( (self.src_dir + a_file), self.dst_dir)
-                    _t = threading.Thread(target=self._move_file_worker, args=(a_file,) )
-                    self._move_thread_list.append(_t)
+                    _t = threading.Thread(target=self._copy_file_worker, args=(a_file,) )
+                    self._copy_thread_list.append(_t)
                     _t.start()
                 else:
-                    print("[MoveQ] Not to move <%s>." % a_file)
+                    print("[copyQ] Not to copy <%s>." % a_file)
                     # The file is already in the list, not doing copying
                     pass
             #
-            if len(self._move_thread_list) > 0:
+            if len(self._copy_thread_list) > 0:
                 self._remove_idle_threads()
-                print("[MoveQ] Number of thread busying = %d" % len(self._move_thread_list) )
+                print("[CopyQ] Number of thread busying = %d" % len(self._copy_thread_list) )
             #
             time.sleep(0.2)
 
@@ -230,8 +229,8 @@ class ROSBAG_CALLER:
             print("The directry <%s> already exists." % self.output_dir_kept)
             pass
 
-        # Initialize the MOVE_QUEUE
-        self.moveQ = MOVE_QUEUE(self.output_dir_tmp, self.output_dir_kept)
+        # Initialize the COPY_QUEUE
+        self.copyQ = COPY_QUEUE(self.output_dir_tmp, self.output_dir_kept)
 
         # File list watcher
         self.file_hist_list = list()
@@ -614,7 +613,7 @@ class ROSBAG_CALLER:
         # Bacuk up "a.bag", note tha empty list is allowed
         for _F in file_in_pre_zone_list:
             # shutil.copy2( (self.output_dir_tmp + _F), self.output_dir_kept)
-            self.moveQ.add_file(_F)
+            self.copyQ.add_file(_F)
 
         """
         # Start a deamon thread for watching the "b.bag"
@@ -647,7 +646,7 @@ class ROSBAG_CALLER:
             (closest_file_name, is_last) = self._get_latest_inactive_bag(_post_trigger_timestamp)
             if (not closest_file_name is None) and not closest_file_name in file_in_pre_zone_list:
                 # shutil.copy2( (self.output_dir_tmp + closest_file_name), self.output_dir_kept)
-                self.moveQ.add_file(closest_file_name)
+                self.copyQ.add_file(closest_file_name)
                 file_in_pre_zone_list.append(closest_file_name)
             if not is_last:
                 break
@@ -662,7 +661,7 @@ class ROSBAG_CALLER:
             if not _F in file_in_pre_zone_list:
                 file_in_pre_zone_list.append(_F)
                 # shutil.copy2( (self.output_dir_tmp + _F), self.output_dir_kept)
-                self.moveQ.add_file(_F)
+                self.copyQ.add_file(_F)
 
         # Write an indication text
         file_in_pre_zone_list.sort()
