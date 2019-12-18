8bc57ea7a5e1a7b8396cd9b3a9b1745944636cfa
chtseng <chtseng@itri.org.tw>
2019-12-18 09:28:33 +0800

    Use ENABLE_CCACHE to make the setting clear.

--- src/cmake/GlobalConfig.cmake
+++ src/cmake/GlobalConfig.cmake
@@ -2,8 +2,9 @@ include(CompilerFlags)
 cmake_policy(SET CMP0043 OLD)
 cmake_policy(SET CMP0046 OLD)
 
+option(ENABLE_CCACHE "Use ccache to speed up build process" ON)
 find_program(CCACHE_PROGRAM ccache)
-if(CCACHE_PROGRAM AND NOT SCAN_BUILD_MODE)
+if(CCACHE_PROGRAM AND ENABLE_CCACHE)
   set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${CCACHE_PROGRAM}")
 endif()
 
