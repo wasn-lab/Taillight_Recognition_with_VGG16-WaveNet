aef5a895d15ec54e5e7219207ca636dd3c77e67c
chtseng <chtseng@itri.org.tw>
2019-12-25 14:51:16 +0800

    Change download url to 118.163.54.109

--- src/dl_data/cmake/drivenet_weights.cmake
+++ src/dl_data/cmake/drivenet_weights.cmake
@@ -1,4 +1,4 @@
 download_file(
-    URL "http://nas.itriadv.co:8888/Share/ADV/S3_git_data/drivenet/yolov3_b1.weights"
+    URL "http://118.163.54.109:8888/Share/ADV/S3_git_data/drivenet/yolov3_b1.weights"
     DESTINATION "${DRIVENET_WEIGHTS_DIR}/yolov3_b1.weights"
     MD5 522efad1446af79900ebf0a7a83ff67a)
