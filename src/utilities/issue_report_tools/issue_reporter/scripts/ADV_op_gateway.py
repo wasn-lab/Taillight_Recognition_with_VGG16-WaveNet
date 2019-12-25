e708407e9786023a5b14c4c5280de2d81842e04c
BensonHuang <BensonHuang@itri.org.tw>
2019-12-25 16:14:01 +0800

    Fix the mode, remove http server in gateway

--- src/utilities/issue_report_tools/issue_reporter/scripts/ADV_op_gateway.py
+++ src/utilities/issue_report_tools/issue_reporter/scripts/ADV_op_gateway.py
@@ -2,7 +2,6 @@
 import random
 import string
 import json
-import cherrypy
 import threading
 #
 
@@ -21,22 +20,12 @@ from std_msgs.msg import (
     Empty,
     Bool,
 )
-from opengl_test.msg import * # test
 #---------------------------#
 
 
 # ROS Publishers
 msg_recorder_trigger_pub = rospy.Publisher('/REC/req_backup', Empty, queue_size=10)
-GUI_cmd_pub = rospy.Publisher('GUI2/operation', GUI2_op, queue_size=10)
-GUI_cmd_pub_seq = 0
 
-# Global variables
-#----------------------------#
-# GUI_op responce
-GUI_state = GUI2_op()
-GUI_state_seq = 0
-GUI_state_seq_old = 0
-#----------------------------#
 
 
 # MQTT
@@ -44,7 +33,7 @@ GUI_state_seq_old = 0
 mqtt_client = None
 mqtt_broker = "127.0.0.1" # localhost
 # MQTT topics
-mqtt_msg_topic_namespace = 'REC'
+mqtt_msg_topic_namespace = 'ADV_op'
 
 # Subscribed topics (MQTT --> ROS)
 #------------------------#
@@ -127,88 +116,12 @@ def REC_report_CB(msg):
 # end ROS callbacks
 
 
-# HTTP server
-#------------------------------------------------------------#
-class GUI_GATEWAY(object):
-    @cherrypy.expose
-    def index(self):
-        return "Hello world!"
-
-    # --- The following is recommended for POST ---
-    # For using POST and read the json through cherrypy.tools.json_in()
-    # then reponse a python dict as json though cherrypy.tools.json_out()
-    @cherrypy.expose
-    @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
-    def json_in_out(self):
-        global GUI_cmd_pub, GUI_cmd_pub_seq
-        global GUI_state, GUI_state_seq, GUI_state_seq_old
-        j_in = None
-        try:
-            j_in = cherrypy.request.json
-        except:
-            print('no json')
-
-        if not j_in is None:
-            print("j_in: " + str(j_in))
-            # print("j_in['data1'] = %s" % j_in['data1'])
-        # Publish to GUI
-        ros_msg = GUI2_op()
-        ros_msg.header.seq = GUI_cmd_pub_seq
-        ros_msg.header.stamp = rospy.get_rostime()
-        ros_msg.gui_name = j_in.get("gui_name", "")
-        ros_msg.cam_view_mode = j_in.get("cam_view_mode", "")
-        ros_msg.cam_motion_mode = j_in.get("cam_motion_mode", "")
-        ros_msg.image3D = j_in.get("image3D", "")
-        ros_msg.image_surr = j_in.get("image_surr", "")
-        ros_msg.cam_op = j_in.get("cam_op", "")
-        ros_msg.record_op = j_in.get("record_op", "")
-        GUI_cmd_pub.publish(ros_msg)
-        GUI_cmd_pub_seq += 1
-        # Trigger backup
-        if ros_msg.record_op == "backup":
-            msg_recorder_trigger_pub.publish( Empty() )
-        #
-
-        # Wait for GUI to response
-        is_received_GUI_state = False
-        start_time = rospy.get_rostime()
-        wait_timeout = rospy.Duration.from_sec(1.0) # Wait for 1 sec.
-        seq_catch = GUI_state_seq 
-        while (rospy.get_rostime() - start_time) < wait_timeout:
-            rospy.sleep(0.01) # Sleep for 0.01 sec.
-            if GUI_state_seq > seq_catch:
-                is_received_GUI_state = True
-                break
-        print("is_received_GUI_state = %s" % str(is_received_GUI_state))
-
-        # Output
-        res_data = dict()
-        res_data["gui_name"] = GUI_state.gui_name
-        res_data["cam_view_mode"] = GUI_state.cam_view_mode
-        res_data["cam_motion_mode"] = GUI_state.cam_motion_mode
-        res_data["image3D"] = GUI_state.image3D
-        res_data["image_surr"] = GUI_state.image_surr
-        # jdata = json.dumps(data)
-        return res_data
-#------------------------------------------------------------#
-# end HTTP server
-
-
-
-
-
-
-
-
-
 
 
 # Wait for ROS terminating signal to close the HTTP server
 def wait_for_close():
-    while not rospy.is_shutdown():
-        rospy.sleep(0.5)
-    cherrypy.engine.exit()
+    pass
+
 
 def main():
     #
@@ -233,7 +146,6 @@ def main():
     # Subscriber callbacks
     mqtt_client.message_callback_add(mqtt_REC_req_backup_subT, mqtt_REC_req_backup_CB)
 
-
     # Connect
     mqtt_client.connect(mqtt_broker, 1883, 60)
     # Start working
@@ -243,12 +155,8 @@ def main():
     # mqtt_client.publish(mqtt_sync_pubT, payload='1', qos=2, retain=False)
     #-------------------------------------------------------------------#
 
-
-    # HTTP server
-    cherrypy.server.socket_host = '0.0.0.0'
-    cherrypy.server.socket_port = 6060
-    cherrypy.server.thread_pool = 10
-    cherrypy.quickstart(GUI_GATEWAY())
+    while not rospy.is_shutdown():
+        rospy.sleep(0.5)
 
     print("End of GUI_gateway")
 
