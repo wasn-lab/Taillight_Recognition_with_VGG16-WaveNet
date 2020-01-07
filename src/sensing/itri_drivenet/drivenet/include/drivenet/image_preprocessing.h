75f1e21db66dbb48774fcc09b58deb03d8525b6c
yiling <amtommy6@gmail.com>
2020-01-07 09:55:19 +0800

    Add cv color variable

--- src/sensing/itri_drivenet/drivenet/include/drivenet/image_preprocessing.h
+++ src/sensing/itri_drivenet/drivenet/include/drivenet/image_preprocessing.h
@@ -9,7 +9,17 @@
 using namespace cv;
 using namespace std;
 
+namespace DriveNet
+{
+class Color{
+    public:
+    static cv::Scalar g_color_blue;
+    static cv::Scalar g_color_red;
+    static cv::Scalar g_color_green;
+    static cv::Scalar g_color_gray;
+};
+
 void loadCalibrationMatrix(String yml_filename, Mat& cameraMatrix, Mat& distCoeffs);
 void calibrationImage(const Mat& src, Mat& dst, Mat cameraMatrix, Mat distCoeffs);
-
+}
 #endif /*IMAGE_PREPROCESSING_H_*/
