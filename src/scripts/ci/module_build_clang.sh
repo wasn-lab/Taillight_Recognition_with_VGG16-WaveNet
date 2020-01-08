dcab733f9adbf5ce55e3fa67b5f6ce80c3e0df8c
chtseng <chtseng@itri.org.tw>
2020-01-08 15:04:43 +0800

    Revert "remove pushd and popd."
    This reverts commit ebb5590fe6ac64bd5ec6517f1e8325735e3b5de2.

--- src/scripts/ci/module_build_clang.sh
+++ src/scripts/ci/module_build_clang.sh
@@ -8,6 +8,7 @@ readonly build_type="${build_type:-Release}"
 readonly repo_dir=$(git rev-parse --show-toplevel)
 readonly build_dir=build_clang
 readonly devel_dir=devel_clang
+pushd $repo_dir
 
 # clean up the previous build.
 for _dir in ${build_dir} ${devel_dir}; do
@@ -24,3 +25,5 @@ catkin_make \
     -DCMAKE_BUILD_TYPE=${build_type} \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
     -DCATKIN_BLACKLIST_PACKAGES="$blacklist" ${EXTRA_CATKIN_ARGS}
+popd
+
