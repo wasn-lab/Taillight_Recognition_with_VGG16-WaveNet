f6307436dd15580f79a70a629b2e28455ac1e9bf
YiLun.Chiu <YiLun.Chiu@itri.org.tw>
2020-01-10 17:00:38 +0800

    Let openpose download from NAS.

--- src/sensing/itri_pedcross/install_openpose.sh
+++ src/sensing/itri_pedcross/install_openpose.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 sudo apt-get install protobuf-compiler libatlas-base-dev 
-git clone https://github.com/CMU-Perceptual-Computing-Lab/openpose.git
+wget http://nas-cht.itriadv.co:8888/Share/ADV/S3_git_data/openpose.tar.gz
+tar zxvf openpose.tar.gz
 cd ./openpose
-git checkout v1.5.1
 mkdir build
 cd ./build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DDOWNLOAD_BODY_25_MODEL=OFF -DDOWNLOAD_FACE_MODEL=OFF -DDOWNLOAD_HAND_MODEL=OFF ..
 make -j
 sudo make install
-
+cd ../..
+rm openpose.tar.gz
