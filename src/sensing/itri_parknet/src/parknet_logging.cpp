e6c2406c54250d50837e0e2591e03cddc8d04cbd
chtseng <chtseng@itri.org.tw>
2020-01-11 21:47:34 +0800

    Revert "Test cuda worker."
    This reverts commit 38240595f2a70b5f5670ac34f56874f223e94537.

--- src/sensing/itri_parknet/src/parknet_logging.cpp
+++ src/sensing/itri_parknet/src/parknet_logging.cpp
@@ -14,7 +14,6 @@ bool do_logging()
 int calc_duration_in_millisecond(std::chrono::time_point<std::chrono::high_resolution_clock> begin,
                                  std::chrono::time_point<std::chrono::high_resolution_clock> end)
 {
-#error "test buildbot worker"
   auto dur = end - begin;
   return std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
 }
