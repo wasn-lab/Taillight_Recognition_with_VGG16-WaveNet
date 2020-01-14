42ef412a048a13e69844b1c692d0ad692567096c
chtseng <chtseng@itri.org.tw>
2020-01-14 13:37:58 +0800

    Add DebPack.cmake for deployment.

--- src/scripts/ci/module_build_clang.sh
+++ src/scripts/ci/module_build_clang.sh
@@ -5,6 +5,7 @@ export CC=clang
 export CXX=clang++
 
 readonly build_type="${build_type:-Release}"
+readonly install_prefix="${install_prefix:-/usr/local/itriadv}"
 readonly repo_dir=$(git rev-parse --show-toplevel)
 readonly build_dir=build_clang
 readonly devel_dir=devel_clang
@@ -22,6 +23,7 @@ blacklist="output_results_by_dbscan;lidar_squseg_inference;lidars_grabber;libs;l
 catkin_make \
     --build ${build_dir} \
     -DCATKIN_DEVEL_PREFIX=${devel_dir} \
+    -DCMAKE_INSTALL_PREFIX=${install_prefix} \
     -DCMAKE_BUILD_TYPE=${build_type} \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
     -DCATKIN_BLACKLIST_PACKAGES="$blacklist" ${EXTRA_CATKIN_ARGS}
