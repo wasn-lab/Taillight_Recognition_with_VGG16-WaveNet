cdc87ddb9c905784b0e9e2a42f49a58146a484b1
yiling <amtommy6@gmail.com>
2020-01-21 14:46:21 +0800

    Add grid map publisher v1

---
+++ src/sensing/itri_drivenet/drivenet/include/drivenet/costmap_generator.h
@@ -0,0 +1,52 @@
+#ifndef COSTMAP_GENERATOR_H_
+#define COSTMAP_GENERATOR_H_
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+// ros - grid map
+#include <grid_map_ros/grid_map_ros.hpp>
+#include <grid_map_ros/GridMapRosConverter.hpp>
+#include <grid_map_msgs/GridMap.h>
+// msgs
+#include <msgs/PointXYZ.h>
+#include <msgs/ConvexPoint.h>
+#include <msgs/DetectedObjectArray.h>
+
+class CosmapGenerator
+{
+private:
+  // grid_map
+  double grid_length_x_;
+  double grid_length_y_;
+  double grid_resolution_;
+  double grid_position_x_;
+  double grid_position_y_;
+  double grid_min_value_;
+  double grid_max_value_;
+  double maximum_lidar_height_thres_;
+  double minimum_lidar_height_thres_;
+
+  // functions
+  msgs::ConvexPoint makePolygonFromObjectBox(const msgs::DetectedObject in_objects);
+  grid_map::Index fetchGridIndexFromPoint(msgs::PointXYZ point);
+  bool isValidInd(const grid_map::Index grid_ind);
+  msgs::PointXYZ makeMidpoint(msgs::PointXYZ p1, msgs::PointXYZ p2);
+
+public:
+  // grid_map
+  std::string layer_name_;
+
+  // functions
+  CosmapGenerator();
+  ~CosmapGenerator();
+  grid_map::GridMap initGridMap();
+  grid_map::Matrix makeCostmapFromObjects(const grid_map::GridMap costmap, const std::string gridmap_layer_name,
+                                          const double expand_polygon_size, const msgs::DetectedObjectArray in_objects,
+                                          const bool use_objects_convex_hull);
+  void objectMsgToGridMapMsg(const msgs::DetectedObjectArray& in_objects, ros::Publisher gridmap_publisher);
+  nav_msgs::OccupancyGrid gridMapToOccupancyMsg(grid_map::GridMap& costmap, std_msgs::Header header);
+  int gridMsgPublisher(grid_map::GridMap& costmap, ros::Publisher gridmap_publisher, std_msgs::Header header);
+  int OccupancyMsgPublisher(grid_map::GridMap& costmap, ros::Publisher occupancy_grid_publisher,
+                            std_msgs::Header header);
+};
+#endif /*COSTMAP_GENERATOR_H_*/
