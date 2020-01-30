18b38fe94becc3975e345e0315bf7e412d667ec9
yiling <amtommy6@gmail.com>
2020-01-30 17:57:38 +0800

    Fix coding style

--- src/sensing/itri_drivenet/convex_fusion/include/convex_fusion_b1.h
+++ src/sensing/itri_drivenet/convex_fusion/include/convex_fusion_b1.h
@@ -39,127 +39,119 @@
 
 class ConvexFusionB1
 {
-  public:
-    static ros::Publisher ErrorCode_pub;
-    static ros::Publisher CameraDetection_pub;
-    
-    static void
-    initial (std::string nodename,
-             int argc,
-             char ** argv)
+public:
+  static ros::Publisher ErrorCode_pub;
+  static ros::Publisher CameraDetection_pub;
+
+  static void initial(std::string nodename, int argc, char** argv)
+  {
+    ros::init(argc, argv, nodename);
+    ros::NodeHandle n;
+
+    ErrorCode_pub = n.advertise<msgs::ErrorCode>("/ErrorCode", 1);
+    CameraDetection_pub = n.advertise<msgs::DetectedObjectArray>(camera::detect_result_polygon, 1);
+  }
+
+  static void RegisterCallBackLidarAllNonGround(void (*cb1)(const pcl::PointCloud<pcl::PointXYZI>::ConstPtr&))
+  {
+    ros::NodeHandle n;
+
+    static ros::Subscriber LidarAllNonGroundSub = n.subscribe("/LidarAll/NonGround", 1, cb1);
+  }
+
+  static void RegisterCallBackCameraDetection(void (*cb1)(const msgs::DetectedObjectArray::ConstPtr&),
+                                              void (*cb2)(const msgs::DetectedObjectArray::ConstPtr&),
+                                              void (*cb3)(const msgs::DetectedObjectArray::ConstPtr&))
+  {
+    ros::NodeHandle n;
+
+    static ros::Subscriber CameraFCDetectionSub = n.subscribe(camera::topics_obj[camera::id::front_60], 1, cb1);
+    static ros::Subscriber CameraFTDetectionSub = n.subscribe(camera::topics_obj[camera::id::top_front_120], 1, cb2);
+    static ros::Subscriber CameraBTDetectionSub = n.subscribe(camera::topics_obj[camera::id::top_rear_120], 1, cb3);
+  }
+
+  static void send_ErrorCode(unsigned int error_code)
+  {
+    static uint32_t seq;
+
+    msgs::ErrorCode objMsg;
+    objMsg.header.seq = seq++;
+    objMsg.header.stamp = ros::Time::now();
+    objMsg.header.frame_id = "lidar";
+    objMsg.module = 2;
+    objMsg.event = error_code;
+
+    ErrorCode_pub.publish(objMsg);
+  }
+
+  static void Send_CameraResults(CLUSTER_INFO* cluster_info, int cluster_size, ros::Time rostime, std::string frameId)
+  {
+    msgs::DetectedObjectArray msgObjArr;
+    float min_z = -3;
+    float max_z = -1.5;
+    for (int i = 0; i < cluster_size; i++)
     {
-      ros::init (argc, argv, nodename);
-      ros::NodeHandle n;
-
-      ErrorCode_pub = n.advertise<msgs::ErrorCode> ("/ErrorCode", 1);
-      CameraDetection_pub = n.advertise<msgs::DetectedObjectArray> (camera::detect_result_polygon, 1);
-    }
-
-    static void
-    RegisterCallBackLidarAllNonGround (void
-    (*cb1) (const pcl::PointCloud<pcl::PointXYZI>::ConstPtr&))
-    {
-      ros::NodeHandle n;
-
-      static ros::Subscriber LidarAllNonGroundSub = n.subscribe ("/LidarAll/NonGround", 1, cb1);
-    }
-  
-    static void
-    RegisterCallBackCameraDetection (void
-                                     (*cb1) (const msgs::DetectedObjectArray::ConstPtr&),
-                                     void
-                                     (*cb2) (const msgs::DetectedObjectArray::ConstPtr&),
-                                     void
-                                     (*cb3) (const msgs::DetectedObjectArray::ConstPtr&))
-    {
-      ros::NodeHandle n;
-
-      static ros::Subscriber CameraFCDetectionSub = n.subscribe (camera::topics_obj[camera::id::front_60], 1, cb1);
-      static ros::Subscriber CameraFTDetectionSub = n.subscribe (camera::topics_obj[camera::id::top_front_120], 1, cb2);
-      static ros::Subscriber CameraBTDetectionSub = n.subscribe (camera::topics_obj[camera::id::top_rear_120], 1, cb3);
-
-    }
-
-    static void
-    send_ErrorCode (unsigned int error_code)
-    {
-      static uint32_t seq;
-
-      msgs::ErrorCode objMsg;
-      objMsg.header.seq = seq++;
-      objMsg.header.stamp = ros::Time::now ();
-      objMsg.header.frame_id = "lidar";
-      objMsg.module = 2;
-      objMsg.event = error_code;
-
-      ErrorCode_pub.publish (objMsg);
-    }
-
-    static void
-    Send_CameraResults (CLUSTER_INFO* cluster_info,
-                        int cluster_size,
-                        ros::Time rostime,
-                        std::string frameId)
-    {
-      msgs::DetectedObjectArray msgObjArr;
-      float min_z = -3;
-      float max_z = -1.5;
-      for (int i = 0; i < cluster_size; i++)
+      msgs::DetectedObject msgObj;
+      msgObj.classId = cluster_info[i].cluster_tag;
+      int convex_hull_size_ = cluster_info[i].convex_hull.size();
+      if (convex_hull_size_ > 0)
       {
-        msgs::DetectedObject msgObj;
-        msgObj.classId = cluster_info[i].cluster_tag;
-        int convex_hull_size_ = cluster_info[i].convex_hull.size();
-        if (convex_hull_size_ > 0)
+        for (size_t j = 0; j < convex_hull_size_; j++)
         {
-          for (size_t j = 0; j < convex_hull_size_; j++)
-          {
-            msgs::PointXYZ p0;
-            p0.x = cluster_info[i].convex_hull[j].x;
-            p0.y = cluster_info[i].convex_hull[j].y;
-            if (cluster_info[i].min.z < min_z) p0.z = min_z;
-            else p0.z = cluster_info[i].min.z;
-            msgObj.cPoint.lowerAreaPoints.push_back(p0);
-          }
-          for (size_t j = 0; j < convex_hull_size_; j++)
-          {
-            msgs::PointXYZ p0;
-            p0.x = cluster_info[i].convex_hull[j].x;
-            p0.y = cluster_info[i].convex_hull[j].y;
-            if (cluster_info[i].max.z > max_z) p0.z = max_z;
-            else p0.z = cluster_info[i].max.z;
-            msgObj.cPoint.lowerAreaPoints.push_back(p0);
-          }
-          for (size_t j = 0; j < convex_hull_size_; j++)
-          {
-            msgs::PointXYZ p0;
-            p0.x = cluster_info[i].convex_hull[j].x;
-            p0.y = cluster_info[i].convex_hull[j].y;
-            if (cluster_info[i].min.z < min_z) p0.z = min_z;
-            else p0.z = cluster_info[i].min.z;
-            msgObj.cPoint.lowerAreaPoints.push_back(p0);
-            p0.x = cluster_info[i].convex_hull[j].x;
-            p0.y = cluster_info[i].convex_hull[j].y;
-            if (cluster_info[i].max.z > max_z) p0.z = max_z;
-            else p0.z = cluster_info[i].max.z;
-            msgObj.cPoint.lowerAreaPoints.push_back(p0);
-          }          
-
-          msgObj.cPoint.objectHigh = cluster_info[i].dz;
-
-          msgObj.fusionSourceId = 0;
-
-          msgObj.header.stamp = rostime;
-          msgObjArr.objects.push_back (msgObj);
+          msgs::PointXYZ p0;
+          p0.x = cluster_info[i].convex_hull[j].x;
+          p0.y = cluster_info[i].convex_hull[j].y;
+          if (cluster_info[i].min.z < min_z)
+            p0.z = min_z;
+          else
+            p0.z = cluster_info[i].min.z;
+          msgObj.cPoint.lowerAreaPoints.push_back(p0);
         }
-      }
-      msgObjArr.header.stamp = rostime;
-      msgObjArr.header.frame_id = frameId;
-      CameraDetection_pub.publish (msgObjArr);
+        for (size_t j = 0; j < convex_hull_size_; j++)
+        {
+          msgs::PointXYZ p0;
+          p0.x = cluster_info[i].convex_hull[j].x;
+          p0.y = cluster_info[i].convex_hull[j].y;
+          if (cluster_info[i].max.z > max_z)
+            p0.z = max_z;
+          else
+            p0.z = cluster_info[i].max.z;
+          msgObj.cPoint.lowerAreaPoints.push_back(p0);
+        }
+        for (size_t j = 0; j < convex_hull_size_; j++)
+        {
+          msgs::PointXYZ p0;
+          p0.x = cluster_info[i].convex_hull[j].x;
+          p0.y = cluster_info[i].convex_hull[j].y;
+          if (cluster_info[i].min.z < min_z)
+            p0.z = min_z;
+          else
+            p0.z = cluster_info[i].min.z;
+          msgObj.cPoint.lowerAreaPoints.push_back(p0);
+          p0.x = cluster_info[i].convex_hull[j].x;
+          p0.y = cluster_info[i].convex_hull[j].y;
+          if (cluster_info[i].max.z > max_z)
+            p0.z = max_z;
+          else
+            p0.z = cluster_info[i].max.z;
+          msgObj.cPoint.lowerAreaPoints.push_back(p0);
+        }
 
+        msgObj.cPoint.objectHigh = cluster_info[i].dz;
+
+        msgObj.fusionSourceId = 0;
+
+        msgObj.header.stamp = rostime;
+        msgObjArr.objects.push_back(msgObj);
+      }
     }
+    msgObjArr.header.stamp = rostime;
+    msgObjArr.header.frame_id = frameId;
+    CameraDetection_pub.publish(msgObjArr);
+  }
 };
 
 ros::Publisher ConvexFusionB1::ErrorCode_pub;
 ros::Publisher ConvexFusionB1::CameraDetection_pub;
 
-#endif // CONVEX_FUSION_B1_H
+#endif  // CONVEX_FUSION_B1_H
