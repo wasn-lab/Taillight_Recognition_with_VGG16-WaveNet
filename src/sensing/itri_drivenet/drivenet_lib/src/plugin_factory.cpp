730eed3b8bd278191a0beb2111dda2d1c3e3c1d1
yiling <amtommy6@gmail.com>
2020-02-04 10:10:11 +0800

    Fix unused variable warning

--- src/sensing/itri_drivenet/drivenet_lib/src/plugin_factory.cpp
+++ src/sensing/itri_drivenet/drivenet_lib/src/plugin_factory.cpp
@@ -1,5 +1,8 @@
 #include "plugin_factory.h"
 #include "trt_utils.h"
+
+#define NO_UNUSED_VAR_CHECK(x) ((void)(x))
+
 namespace DriveNet
 {
 PluginFactory::PluginFactory() : m_ReorgLayer{ nullptr }, m_RegionLayer{ nullptr }
@@ -85,6 +88,7 @@ YoloLayerV3::YoloLayerV3(const void* data, size_t length)
   read(d, m_GridSize);
   read(d, m_OutputSize);
   assert(d = a + length);
+  NO_UNUSED_VAR_CHECK(a);  // silence -Wunused-variable
 };
 
 YoloLayerV3::YoloLayerV3(const uint& numBoxes, const uint& numClasses, const uint& gridSize)
@@ -149,5 +153,6 @@ void YoloLayerV3::serialize(void* buffer)
   write(d, m_GridSize);
   write(d, m_OutputSize);
   assert(d == a + getSerializationSize());
+  NO_UNUSED_VAR_CHECK(a);  // silence -Wunused-variable
 }
 }
