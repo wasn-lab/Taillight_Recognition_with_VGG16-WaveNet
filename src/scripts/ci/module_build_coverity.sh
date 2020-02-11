8b039f08245ba8a753d3fca5705117e56269d7c5
chtseng <chtseng@itri.org.tw>
2020-02-11 15:01:40 +0800

    Add blacklisted package in coverity build.

--- src/scripts/ci/module_build_coverity.sh
+++ src/scripts/ci/module_build_coverity.sh
@@ -11,31 +11,52 @@ if [[ -z "${cov_build_bin}" ]]; then
 fi
 
 readonly coverity_data_dir="coverity_data"
+readonly coverity_data_blacklist_dir="coverity_data_blacklist"
 readonly coverity_root=$(readlink -e $(dirname ${cov_build_bin})/..)
-readonly cfgs="${coverity_root}/config/MISRA/MISRA_c2004_7.config ${coverity_root}/config/MISRA/MISRA_c2012_7.config ${coverity_root}/config/MISRA/MISRA_cpp2008_7.config
-"
+readonly cfgs="${coverity_root}/config/MISRA/MISRA_c2004_7.config
+               ${coverity_root}/config/MISRA/MISRA_c2012_7.config
+               ${coverity_root}/config/MISRA/MISRA_cpp2008_7.config"
+readonly blacklist="localization;ndt_gpu"
+
 pushd $repo_dir
 
-# clean up the previous build.
-for _dir in build devel ${coverity_data_dir}; do
-    if [[ -d $_dir ]]; then
-        rm -rf $_dir
-    fi
-done
+function cleanup {
+  for _dir in build devel ${coverity_data_dir} ${coverity_data_blacklist_dir}; do
+      if [[ -d $_dir ]]; then
+          rm -rf $_dir
+      fi
+  done
+}
 
-blacklist="localization;ndt_gpu"
+function commit {
+  data_dir=$1
+  if [[ ! -z "${USER_NAME}" && ! -z "${USER_PASSWORD}" ]]; then
+    cov-commit-defects --host 140.96.109.174 --dataport 9090 --stream master --dir ${data_dir} --user ${USER_NAME} --password ${USER_PASSWORD}
+  fi
+}
 
+function analyze {
+  data_dir=$1
+  cov-analyze -dir ${data_dir} --strip-path ${repo_dir}/
+  for cfg in $cfgs; do
+    cov-analyze --disable-default --misra-config ${cfg} --dir ${data_dir} --strip-path ${repo_dir}/
+  done
+}
+
+# build blacklisted packages
+cleanup
+cov-build --dir ${coverity_data_blacklist_dir} --emit-complementary-info \
+  catkin_make -DENABLE_CCACHE=0 -DCATKIN_WHITELIST_PACKAGES="${blacklist};cuda_downsample" ${EXTRA_CATKIN_ARGS}
+analyze ${coverity_data_blacklist_dir}
+commit ${coverity_data_blacklist_dir}
+
+# build all except blacklisted packages
+# Use this order because most people can see analysis result at the most recent stream.
+cleanup
 cov-build --dir ${coverity_data_dir} --emit-complementary-info \
-catkin_make -DENABLE_CCACHE=0 -DCATKIN_BLACKLIST_PACKAGES="$blacklist" ${EXTRA_CATKIN_ARGS} -j`nproc`
+  catkin_make -DENABLE_CCACHE=0 -DCATKIN_BLACKLIST_PACKAGES="$blacklist" ${EXTRA_CATKIN_ARGS}
+analyze ${coverity_data_dir}
+commit ${coverity_data_dir}
 
-cov-analyze -dir ${coverity_data_dir} --strip-path ${repo_dir}/
-for cfg in $cfgs; do
-  cov-analyze --disable-default --misra-config ${cfg} --dir coverity_data --strip-path ${repo_dir}/
-done
-
-if [[ ! -z "${USER_NAME}" && ! -z "${USER_PASSWORD}" ]]; then
-  cov-commit-defects --host 140.96.109.174 --dataport 9090 --stream master --dir coverity_data --user ${USER_NAME} --password ${USER_PASSWORD}
-  echo "visit http://140.96.109.174:8080 to see the result (project: itriadv, stream: master)"
-fi
-
+echo "visit http://140.96.109.174:8080 to see the result (project: itriadv, stream: master)"
 popd
