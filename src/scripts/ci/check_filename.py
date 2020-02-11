9b3716d76669a686f64cf8a6c87ca09a9f6c1350
chtseng <chtseng@itri.org.tw>
2020-02-11 16:07:45 +0800

    Ban hpp files as people are inclined to use the style.

--- src/scripts/ci/check_filename.py
+++ src/scripts/ci/check_filename.py
@@ -30,10 +30,19 @@ def _check_space(affected_files):
         print("file name check passed.")
     return len(fnames_with_space)
 
+def _check_hpp(affected_files):
+    fnames_hpp = []
+    for fname in affacted_files:
+        if fname.endswith(".hpp"):
+            fnames_hpp.append(fname)
+    if fnames_hpp:
+        logging.error("ROS header files are named after .h, not.hpp: %s", " ".join(fnames_hpp))
+    return len(fnames_hpp)
+
 def main():
     """Prog entry"""
     affected_files = _get_affected_files()
-    return _check_space(affected_files)
+    return _check_space(affected_files) + _check_hpp(affected_files)
 
 if __name__ == "__main__":
     sys.exit(main())
