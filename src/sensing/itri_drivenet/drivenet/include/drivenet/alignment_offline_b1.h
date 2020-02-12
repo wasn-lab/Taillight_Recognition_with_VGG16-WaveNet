15c99d5fe9847293cef1feb357e35e30c9f09416
Mike Lin <mike19931010@gmail.com>
2020-02-12 15:07:08 +0800

    Update code for new alignment offline

--- src/sensing/itri_drivenet/drivenet/include/drivenet/alignment_offline_b1.h
+++ src/sensing/itri_drivenet/drivenet/include/drivenet/alignment_offline_b1.h
@@ -2,21 +2,33 @@
 #define ALIGNMENT_OFFLINE_H_
 
 // ROS message
+#include "ros/ros.h"
+#include <std_msgs/String.h>
+#include <std_msgs/Header.h>
+#include "sensor_msgs/PointCloud2.h"
+
 #include "camera_params.h"  // include camera topic name
 #include <msgs/BoxPoint.h>
 #include <msgs/PointXYZ.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "projection/projector2.h"
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
 
 
 class AlignmentOff
 {
 private:
   int carId = 1;
+  Projector2 pj;
 
 public:
   void init(int carId);
-
+  vector<int> run();  
+  vector<int> out;
+  
 };
 
 #endif /*ALIGNMENT_OFFLINE_H_*/
--- src/sensing/itri_drivenet/drivenet/src/alignment_offline_b1.cpp
+++ src/sensing/itri_drivenet/drivenet/src/alignment_offline_b1.cpp
@@ -6,5 +6,35 @@
 void AlignmentOff::init(int car_id)
 {
   carId = car_id;
+  pj.init(camera::id::front_60);
 }
 
+vector<int> AlignmentOff::run()
+{
+  return pj.project(1,2,3);
+}
+
+void callback_LidarAll(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+  pcl::PointCloud<pcl::PointXYZI>::Ptr LidAll_cloudPtr(new pcl::PointCloud<pcl::PointXYZI>);  
+  pcl::fromROSMsg(*msg, *LidAll_cloudPtr);
+  std::cout << "success" << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+  // new
+  ros::init(argc, argv, "alignmentOff");
+  ros::NodeHandle nh;
+
+  ros::Subscriber LidarSc;
+  LidarSc = nh.subscribe("LidarAll", 1, callback_LidarAll);
+
+  AlignmentOff al;
+  al.init(1);
+  al.out = al.run();
+
+  ros::spin();
+
+  return 0;
+}
