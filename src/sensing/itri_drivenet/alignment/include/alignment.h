9840062b76568201b0b2b5a212ed71be338b434f
yiling <amtommy6@gmail.com>
2020-02-14 15:05:38 +0800

    Add 3D points on image visualization

--- src/sensing/itri_drivenet/alignment/include/alignment.h
+++ src/sensing/itri_drivenet/alignment/include/alignment.h
@@ -1,14 +1,32 @@
 #ifndef ALIGNMENT_H_
 #define ALIGNMENT_H_
 
-//projection
+/// pcl
+#include <pcl/point_types.h>
+
+/// projection
 #include <projection/projector2.h>
+
+///util
+#include <camera_params.h>
+
+struct PixelPosition
+{
+	int u;
+	int v;
+};
 
 class Alignment
 {
 private:	
-	Projector2 projector2;
-
+	Projector2 projector2_;
+	int image_w_ = camera::image_width;
+	int image_h_ = camera::image_height;
+public:
+	Alignment();
+	~Alignment();
+	void projectMatrixInit(int cam_id);
+	PixelPosition projectPointToPixel(pcl::PointXYZI point);
 };
 
 #endif
