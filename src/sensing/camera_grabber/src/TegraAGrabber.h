d84f90b36b3078b009bceb2e93d81218049ab666
yiling <amtommy6@gmail.com>
2020-03-02 10:39:25 +0800

    Fix camera_grabber camera id

--- src/sensing/camera_grabber/src/TegraAGrabber.h
+++ src/sensing/camera_grabber/src/TegraAGrabber.h
@@ -18,11 +18,11 @@ protected:
   void InitParameters();
 #if CAR_MODEL_IS_B1
   // TODO: fill in the correct camera id.
-  const std::vector<int> cam_ids_{ camera::id::right_60,  camera::id::front_60,  camera::id::left_60 };
+  const std::vector<int> cam_ids_{ camera::id::right_60,  camera::id::front_bottom_60,  camera::id::left_60 };
 #elif CAR_MODEL_IS_B1_V2
-  const std::vector<int> cam_ids_{ camera::id::front_60,  camera::id::top_front_30};
+  const std::vector<int> cam_ids_{ camera::id::front_bottom_60,  camera::id::top_front_30};
 #elif CAR_MODEL_IS_HINO
-  const std::vector<int> cam_ids_{ camera::id::left_60, camera::id::front_60, camera::id::right_60,
+  const std::vector<int> cam_ids_{ camera::id::left_60, camera::id::front_bottom_60, camera::id::right_60,
                                    camera::id::left_30, camera::id::front_30, camera::id::right_30 };
 #else
 #error "car model is not well defined"
