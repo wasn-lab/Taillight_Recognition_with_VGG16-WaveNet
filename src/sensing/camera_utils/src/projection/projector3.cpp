dfc28b004a26b37789b74ae83b0f7d83474e5184
Ching-Hao Liu <chinghao.liu@itri.org.tw>
2020-03-04 18:29:10 +0800

    Apply coding style.

--- src/sensing/camera_utils/src/projection/projector3.cpp
+++ src/sensing/camera_utils/src/projection/projector3.cpp
@@ -5,24 +5,24 @@
 
 void Projector3::init(const char* camera_topic_name)
 {
-  if(strcmp(camera_topic_name,"/cam/F_center") == 0)
+  if (strcmp(camera_topic_name, "/cam/F_center") == 0)
   {
-    char* fileName = (char *)"test_0225_F_center.yml";
+    char* fileName = (char*)"test_0225_F_center.yml";
     char* filePath = new char[std::strlen(CAMERA_UTILS_DATA_DIR) + std::strlen(fileName) + 1];
-    std::strcpy(filePath,CAMERA_UTILS_DATA_DIR);
-    std::strcat(filePath,fileName);
+    std::strcpy(filePath, CAMERA_UTILS_DATA_DIR);
+    std::strcat(filePath, fileName);
     readCameraParameters(filePath);
   }
   else
   {
-    std::cerr << " No match topic name, init failed."<< std::endl;
+    std::cerr << " No match topic name, init failed." << std::endl;
   }
 }
 
 std::vector<int> Projector3::project(double x, double y, double z)
 {
   std::vector<int> result(2);
-  if(!projectionMatrix.empty())
+  if (!projectionMatrix.empty())
   {
     std::vector<cv::Point3d> objectPoint;
     objectPoint.push_back(cv::Point3d(x, y, z));
@@ -33,37 +33,37 @@ std::vector<int> Projector3::project(double x, double y, double z)
   }
   else
   {
-    std::cerr << " Projection Matrix is empty."<< std::endl;
+    std::cerr << " Projection Matrix is empty." << std::endl;
   }
   return result;
 }
 
-void Projector3::readCameraParameters(const char* yml_filename )
+void Projector3::readCameraParameters(const char* yml_filename)
 {
   cv::FileStorage fs;
   fs.open(yml_filename, cv::FileStorage::READ);
-  if( !fs.isOpened() ){
+  if (!fs.isOpened())
+  {
     std::cerr << " Failed to open " << yml_filename << std::endl;
     exit(EXIT_FAILURE);
   }
   fs["CameraExtrinsicMat"] >> cameraExtrinsicMat;
-  rotarionMat = cameraExtrinsicMat(cv::Range(0,3), cv::Range(0,3)).t();
-  //cv::Mat R = angleToRotation(M_PI_2, -M_PI_2, 0.0f);
-  //rotarionMat = rotarionMat*R;
+  rotarionMat = cameraExtrinsicMat(cv::Range(0, 3), cv::Range(0, 3)).t();
+  // cv::Mat R = angleToRotation(M_PI_2, -M_PI_2, 0.0f);
+  // rotarionMat = rotarionMat*R;
   cv::Rodrigues(rotarionMat, rotarionVec);
-  double x = cameraExtrinsicMat.at<double>(1,3);
-  double y = cameraExtrinsicMat.at<double>(2,3);
-  double z = -cameraExtrinsicMat.at<double>(0,3);
+  double x = cameraExtrinsicMat.at<double>(1, 3);
+  double y = cameraExtrinsicMat.at<double>(2, 3);
+  double z = -cameraExtrinsicMat.at<double>(0, 3);
   translationVec = cv::Mat::zeros(3, 1, cv::DataType<double>::type);
-  translationVec.at<double>(0,0) = x;
-  translationVec.at<double>(1,0) = y;
-  translationVec.at<double>(2,0) = z;
+  translationVec.at<double>(0, 0) = x;
+  translationVec.at<double>(1, 0) = y;
+  translationVec.at<double>(2, 0) = z;
   fs["CameraMat"] >> cameraMat;
-  fs["DistCoeff"] >> distCoeff; 
+  fs["DistCoeff"] >> distCoeff;
   fs["ImageSize"] >> ImageSize;
-  cv::hconcat(rotarionMat,translationVec,projectionMatrix);
-  projectionMatrix = cameraMat*projectionMatrix;
-  
+  cv::hconcat(rotarionMat, translationVec, projectionMatrix);
+  projectionMatrix = cameraMat * projectionMatrix;
 
   // Debug: Camera Parameters
   /*
