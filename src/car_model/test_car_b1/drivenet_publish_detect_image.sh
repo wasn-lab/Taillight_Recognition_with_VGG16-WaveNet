74d82bc8ef2a01509a2d85858f2ee4fad034d4fd
chtseng <chtseng@itri.org.tw>
2020-03-04 10:02:47 +0800

    Add car model check in tests.

--- src/car_model/test_car_b1/drivenet_publish_detect_image.sh
+++ src/car_model/test_car_b1/drivenet_publish_detect_image.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -x
 set -e
+source build/car_model/scripts/car_model.sh
+if [[ "${CAR_MODEL}" != "B1" ]]; then
+  echo "This script is for B1 only."
+  exit 0
+fi
 
 source devel/setup.bash
 catkin_make run_tests_car_model_rostest_test_car_b1_drivenet_publish_detect_image.test
