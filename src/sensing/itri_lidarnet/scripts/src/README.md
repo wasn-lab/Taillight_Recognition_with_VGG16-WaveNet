837bc82b0bc9719ae839eb0977459e6da002d2ee
MikeChan <m7807031@gmail.com>
2020-03-16 15:25:04 +0800

    Add ipc_set_br0.sh for Ubuntu18.04 bridge setting.

--- src/sensing/itri_lidarnet/scripts/src/README.md
+++ src/sensing/itri_lidarnet/scripts/src/README.md
@@ -1,3 +1,9 @@
+## ipc_set_br0.sh
+Create bro bridge for Lidar driver connection.
+```bash
+sudo bash ipc_set_br0.sh
+```
+
 ## ipc_set_dhcp_server.sh
 Enable dhcp by dnsmasq, then do some Ouster Lidar setting.
 ```bash
---
+++ src/sensing/itri_lidarnet/scripts/src/ipc_set_br0.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+sudo ifdown -a
+
+
+# enter ifconfig info
+ls /sys/class/net/
+echo ""
+read -p "Enter interface_names (split by space): " interface_names
+IFS=' ' read -a interface_names_array <<< "$interface_names"  
+
+for index in ${!interface_names_array[@]}
+do  
+    echo "$index : ${interface_names_array[index]}"  
+    line+="${interface_names_array[index]} "
+done 
+echo "interface counts: ${#interface_names_array[@]}"  
+#echo "bridge_ports $line"
+
+
+# create br0 for ouster
+sudo bash -c "echo '' >> /etc/network/interfaces"
+sudo bash -c "echo '# create br0 for ouster' >> /etc/network/interfaces"
+sudo bash -c "echo 'auto br0' >> /etc/network/interfaces"
+sudo bash -c "echo 'iface br0 inet static' >> /etc/network/interfaces"
+sudo bash -c "echo 'address 192.168.0.1' >> /etc/network/interfaces"
+sudo bash -c "echo 'netmask 255.255.255.0' >> /etc/network/interfaces"
+sudo bash -c "echo 'network 192.168.0.1' >> /etc/network/interfaces"
+sudo bash -c "echo 'broadcast 255.255.255.255' >> /etc/network/interfaces"
+sudo bash -c "echo 'gateway 192.168.0.1' >> /etc/network/interfaces"
+sudo bash -c "echo 'dns-nameservers 8.8.8.8 8.8.4.4' >> /etc/network/interfaces"
+sudo bash -c "echo 'dns-search localdomain.local' >> /etc/network/interfaces"
+sudo bash -c "echo 'bridge_ports $line' >> /etc/network/interfaces"
+
+
+sudo ifup -a
