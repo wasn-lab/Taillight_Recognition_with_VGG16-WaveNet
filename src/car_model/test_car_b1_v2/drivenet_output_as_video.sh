93dc365bf130c80a78a4402e694d4bcdecd17d3e
chtseng <chtseng@itri.org.tw>
2020-03-20 14:10:41 +0800

    Allow to use browser to view video artifacts.

--- src/car_model/test_car_b1_v2/drivenet_output_as_video.sh
+++ src/car_model/test_car_b1_v2/drivenet_output_as_video.sh
@@ -11,5 +11,17 @@ fi
 source devel/setup.bash
 export output_dir=/var/www/html/artifacts/$(date "+%Y%m%d")
 mkdir -p $output_dir
+chmod 775 $output_dir
 
 roslaunch car_model drivenet_output_as_video.test
+
+pushd $output_dir
+for avi in `ls *.avi`; do
+  webm=$(basename $avi .avi).webm
+  ffmpeg -y -i ${avi} $webm
+done
+popd
+
+cp src/scripts/ci/show_drivenet_videos.html $output_dir
+
+echo "Visit http://ci.itriadv.co/artifacts/${output_dir} to download the results."
