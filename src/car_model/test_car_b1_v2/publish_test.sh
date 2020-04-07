9e05559b426ffb9f4a49938eb5f13f31982ad7bd
chtseng <chtseng@itri.org.tw>
2020-04-07 09:31:46 +0800

    Use publish_test.sh to unify the same kind of test cases.

--- src/car_model/test_car_b1_v2/publish_test.sh
+++ src/car_model/test_car_b1_v2/publish_test.sh
@@ -4,9 +4,10 @@ set -e
 source build/car_model/scripts/car_model.sh
 if [[ "${CAR_MODEL}" != "B1_V2" ]]; then
   echo "This script is for B1_V2 only."
-  exit 0
+  exit 1
 fi
 
 source devel/setup.bash
-catkin_make run_tests_car_model_rostest_test_car_b1_v2_drivenet_publish_detect_image.test
 
+rostest car_model publish_test_drivenet_b1_v2.test
+rostest car_model publish_test_tpp_b1_v2.test
