0a3bb9771ee933eb200b9414a1c58d8b693d97fd
chtseng <chtseng@itri.org.tw>
2020-04-08 10:50:41 +0800

    Create test env if needed.

--- src/car_model/test_car_b1_v2/publish_test.sh
+++ src/car_model/test_car_b1_v2/publish_test.sh
@@ -7,6 +7,10 @@ if [[ "${CAR_MODEL}" != "B1_V2" ]]; then
   exit 1
 fi
 
+if [[ ! -f src/sensing/itri_drivenet/drivenet/data/yolo/yolov3_b1-kINT8-batch3.engine ]]; then
+  bash src/car_model/test_car_b1_v2/init_test_env.sh
+fi
+
 source devel/setup.bash
 
 rostest car_model publish_test_drivenet_b1_v2.test
