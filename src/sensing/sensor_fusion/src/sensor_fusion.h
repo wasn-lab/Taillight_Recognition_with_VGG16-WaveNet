d62c9cd8500066c6f837c5f1cccc1bd3ae80800d
Ching-Hao Liu <chinghao.liu@itri.org.tw>
2020-04-09 12:01:32 +0800

    Add debug msg and modularize.

--- src/sensing/sensor_fusion/src/sensor_fusion.h
+++ src/sensing/sensor_fusion/src/sensor_fusion.h
@@ -9,8 +9,8 @@
 
 #include "hungarian.h"
 
-#define DEBUG 0
-#define OBJCLASS_SOURCE 1  // 0: lidar; 1: camera
+#define DEBUG_HUNGARIAN 0
+#define DEBUG_OBJCLASS 0
 /************************************************************************/
 ros::Subscriber lidar_sub;
 ros::Subscriber camera_sub;
@@ -33,6 +33,10 @@ std::vector<std::vector<double> > distance_table_;
 void get_obj_center(double& obj_cx, double obj_cy, const msgs::DetectedObject& obj);
 void init_distance_table(std::vector<msgs::DetectedObject>& objs1, std::vector<msgs::DetectedObject>& objs2);
 void associate_data(std::vector<msgs::DetectedObject>& objs1, std::vector<msgs::DetectedObject>& objs2);
+/************************************************************************/
+const int fusion_objclass_source = 0;  // 0: camera; 1: lidar
+void fuseObjClass(uint16_t obj1_class, const size_t obj1_id, const std::string obj1_source, const uint16_t obj2_class,
+                  const size_t obj2_id, const std::string obj2_source);
 /************************************************************************/
 void MySigintHandler(int sig);
 int main(int argc, char** argv);
