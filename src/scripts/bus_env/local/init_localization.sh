5b00ead610f84a5dc35677686ea02fbb65ef6432
austin <austin.chen@itri.org.tw>
2020-04-30 09:56:06 +0800

    add start up script

---
+++ src/scripts/bus_env/local/init_localization.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+source /home/localization/itriadv/devel/setup.bash
+#source /home/localization/tmp/itriadv/devel/setup.bash
+
+export ROS_MASTER_URI=http://192.168.1.3:11311
+export ROS_IP=192.168.1.5
+
+roslaunch localization localization.launch
