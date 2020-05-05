cfd1ac2bd0b5fcd96dda4e3189303806568f7dae
chtseng <chtseng@itri.org.tw>
2020-05-05 10:35:19 +0800

    Use cv::setNumThreads to reduce CPU load.

--- src/sensing/deeplab/src/main.cpp
+++ src/sensing/deeplab/src/main.cpp
@@ -3,12 +3,14 @@
 #include <gflags/gflags_gflags.h>
 #include <glog/logging.h>
 #include <ros/ros.h>
+#include <opencv2/core/utility.hpp>
 
 int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "deeplab");
   gflags::ParseCommandLineFlags(&argc, &argv, false);
   google::InstallFailureSignalHandler();
+  cv::setNumThreads(0);  // Avoid excessive CPU load
   deeplab::DeeplabNode deeplab_node;
 
   deeplab_node.run(argc, argv);
