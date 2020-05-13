9cc2891396cae5e18b847398bf43aa474e6e7552
bshuang <bshuang@itri.org.tw>
2020-05-13 22:01:31 +0800

    add recording scripts and modify some of the scripts

--- src/scripts/bus_env/lidar/init_lidar_record.sh
+++ src/scripts/bus_env/lidar/init_lidar_record.sh
@@ -2,6 +2,8 @@
 source /home/lidar/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.3
-roslaunch lidar b1.launch mode:=2
+roslaunch lidar b1.launch mps:=1 mode:=2
+sleep 1 
+#roslaunch lidar b1.launch hardware_enable:=0
 #roslaunch sdb lidar.launch
 
