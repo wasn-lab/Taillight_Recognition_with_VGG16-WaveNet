8d9eb4649a650afdc99eb9cb4ebfcaae13fd10d4
YiLingWu <amtommy6@gmail.com>
2020-05-27 13:23:03 +0800

    Add distance line on pointcloud and image

--- src/sensing/itri_drivenet/alignment/src/alignment_visualization_b1_v2.cpp
+++ src/sensing/itri_drivenet/alignment/src/alignment_visualization_b1_v2.cpp
@@ -14,6 +14,7 @@
 #include <drivenet/object_label_util.h>
 #include "point_preprocessing.h"
 #include "points_in_image_area.h"
+#include "point_preprocessing.h"
 
 /// opencv
 #include <opencv2/core/core.hpp>
@@ -213,6 +214,27 @@ void displayLidarData()
   pclInitializer(cams_points_ptr);
   pointsColorInit(rgb_cams_points, g_cams_points_ptr);
 
+  MinMax3D point_50m, point_40m, point_30m, point_20m, point_10m;
+  cv::Scalar color_50m, color_40m, color_30m, color_20m, color_10m;
+  float x_dist = 50;
+  float y_dist = 50;
+  float z_dist = -3;
+  point_50m = g_visualization.getDistLinePoint(x_dist, y_dist, z_dist);
+  color_50m = g_visualization.getDistColor(x_dist);
+  x_dist -= 10;
+  point_40m = g_visualization.getDistLinePoint(x_dist, y_dist, z_dist);
+  color_40m = g_visualization.getDistColor(x_dist);
+  x_dist -= 10;
+  point_30m = g_visualization.getDistLinePoint(x_dist, y_dist, z_dist);
+  color_30m = g_visualization.getDistColor(x_dist);
+  x_dist -= 10;
+  point_20m = g_visualization.getDistLinePoint(x_dist, y_dist, z_dist);
+  color_20m = g_visualization.getDistColor(x_dist);
+  x_dist -= 10;
+  point_10m = g_visualization.getDistLinePoint(x_dist, y_dist, z_dist);
+  color_10m = g_visualization.getDistColor(x_dist);
+  std::cout << "point_10m: " << point_10m.p_max.x << ", " << point_10m.p_max.y << std::endl;
+
   /// main loop
   ros::Rate loop_rate(10);
   while (ros::ok() && !pcl_viewer->wasStopped())
@@ -229,6 +251,12 @@ void displayLidarData()
     // std::lock_guard<std::mutex> lock_lidar_process(g_mutex_lidar_process);
     // pcl_viewer->addPointCloud<pcl::PointXYZI>(g_lidarall_ptr_process, rgb_lidarall, "Cloud viewer");  //,
     // viewports[0]);
+
+    pcl_viewer->addLine<pcl::PointXYZI> (point_50m.p_min, point_50m.p_max, color_50m[0], color_50m[1], color_50m[2], "line-50m");
+    pcl_viewer->addLine<pcl::PointXYZI> (point_40m.p_min, point_40m.p_max, color_40m[0], color_40m[1], color_40m[2], "line-40m");
+    pcl_viewer->addLine<pcl::PointXYZI> (point_30m.p_min, point_30m.p_max, color_30m[0], color_30m[1], color_30m[2], "line-30m");
+    pcl_viewer->addLine<pcl::PointXYZI> (point_20m.p_min, point_20m.p_max, color_20m[0], color_20m[1], color_20m[2], "line-20m");
+    pcl_viewer->addLine<pcl::PointXYZI> (point_10m.p_min, point_10m.p_max, color_10m[0], color_10m[1], color_10m[2], "line-10m");
 
     for (size_t cam_order = 0; cam_order < g_cam_ids.size(); cam_order++)
     {
