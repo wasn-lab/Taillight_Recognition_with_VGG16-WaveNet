4e098f8d4e09c044780909b08d34cc88df6736c1
chtseng <chtseng@itri.org.tw>
2020-05-27 10:30:19 +0800

    Add nn_labels.py.

---
+++ src/scripts/weakness_detection/nn_labels.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Definition of labels of neural networks"""
+
+
+class DeeplabLabel:
+    BUS = 6
+    CAR = 7
+    MOTORBIKE = 14
+    PERSON = 15
+
+
+class YoloLabel:
+    PERSON = 0
+    BICYCLE = 1
+    CAR = 2
+    MOTORBIKE = 3
+    BUS = 5
+    TRUCK = 7
+    TRAFFIC_LIGHT = 9
