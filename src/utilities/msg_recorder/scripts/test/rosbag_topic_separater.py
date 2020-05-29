dcd3185933f3d7a6debd5915a18f005db35a7244
BensonHuang <BensonHuang@itri.org.tw>
2020-05-29 15:59:36 +0800

    Add prints

--- src/utilities/msg_recorder/scripts/test/rosbag_topic_separater.py
+++ src/utilities/msg_recorder/scripts/test/rosbag_topic_separater.py
@@ -4,3 +4,8 @@ topics = bag.get_type_and_topic_info()[1].keys()
 types = []
 for i in range(0,len(bag.get_type_and_topic_info()[1].values())):
     types.append(bag.get_type_and_topic_info()[1].values()[i][0])
+
+print("bag.get_type_and_topic_info() = %s" % str(bag.get_type_and_topic_info()))
+
+print("topics = %s" % str(topics))
+print("types = %s" % str(types))
