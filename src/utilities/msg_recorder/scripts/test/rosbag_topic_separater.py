a63ed93462b966ba5d56c82103854b258a760a70
BensonHuang <BensonHuang@itri.org.tw>
2020-05-29 15:05:02 +0800

    Add a test script for separating the rosbag by topics

---
+++ src/utilities/msg_recorder/scripts/test/rosbag_topic_separater.py
@@ -0,0 +1,6 @@
+import rosbag
+bag = rosbag.Bag('input.bag')
+topics = bag.get_type_and_topic_info()[1].keys()
+types = []
+for i in range(0,len(bag.get_type_and_topic_info()[1].values())):
+    types.append(bag.get_type_and_topic_info()[1].values()[i][0])
