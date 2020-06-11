69940d5b4aff7d6663c1f145d90a6d85b6fccbf8
chtseng <chtseng@itri.org.tw>
2020-06-11 14:23:06 +0800

    Disable pylint no-member check.

--- src/utilities/rosbag_extractor/Batch_Rosbag_Extractor.py
+++ src/utilities/rosbag_extractor/Batch_Rosbag_Extractor.py
@@ -30,7 +30,7 @@ check_first = True
 
 def read_topic_return_msg(msg, type, contain):
     if type == 0:  # lidar
-	pc = pypcd.PointCloud.from_msg(msg)
+        pc = pypcd.PointCloud.from_msg(msg)  # pylint: disable=no-member
         return pc
     # if type == 1: #cam
     #     try:
