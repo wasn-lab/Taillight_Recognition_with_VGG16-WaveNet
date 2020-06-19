1a1a2d1a4f1cd8cc825d1b0659ef8e4440419c74
chiaueng <chiaueng@itri.org.tw>
2020-06-19 11:07:15 +0800

    Move the url path of total_map.pcd

--- src/dl_data/cmake/localization.cmake
+++ src/dl_data/cmake/localization.cmake
@@ -1,5 +1,5 @@
 download_file(
-    URL "http://nas.itriadv.co:8888/git_data/Lidar_maps/kinway_itri/lanelet2/total_map.pcd"
+    URL "http://nas.itriadv.co:8888/git_data/B1/lidar_maps/kinway_itri/lanelet2/total_map.pcd"
 
     DESTINATION "${LOCALIZATION_DIR}/src/map_pub/done_map/total_map.pcd"
     MD5 fd75c301ff77b92b836ac81a92b29f7c)
