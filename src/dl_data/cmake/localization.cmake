9762aa3f2e8a2fe0321e750c9cfc74ad5fe6e1bf
chiaueng <chiaueng@itri.org.tw>
2020-06-19 10:55:28 +0800

    Update the URL for total_map.pcd

--- src/dl_data/cmake/localization.cmake
+++ src/dl_data/cmake/localization.cmake
@@ -1,8 +1,6 @@
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_maps/kingway_itri/voxel_0.6/total_map.pcd"
+    URL "http://nas.itriadv.co:8888/git_data/Lidar_maps/kinway_itri/lanelet2/total_map.pcd"
+
     DESTINATION "${LOCALIZATION_DIR}/src/map_pub/done_map/total_map.pcd"
-<<<<<<< HEAD
-    MD5 f3271c597c7018e342cb5cb5e10700ff)
-=======
-    MD5 0680ec48465b98092d77dab40539f3a3)
->>>>>>> remotes/origin/control_team_billhou
+    MD5 fd75c301ff77b92b836ac81a92b29f7c)
+
