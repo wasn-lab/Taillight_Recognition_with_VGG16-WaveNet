3ab1769cd858c639cdb94604dfa7dd273303c706
YiLun.Chiu <YiLun.Chiu@itri.org.tw>
2020-06-21 23:10:06 +0800

    Change models and openpose path on NAS.

--- src/sensing/itri_pedcross/install_openpose.sh
+++ src/sensing/itri_pedcross/install_openpose.sh
@@ -3,9 +3,12 @@
 sudo apt-get install protobuf-compiler libatlas-base-dev 
 # openpose version: commit 825f0d0
 # caffe version: commit b5ede48
-wget http://nas-cht.itriadv.co:8888/Share/ADV/S3_git_data/openpose.tar.gz
+wget http://nas.itriadv.co:8888/git_data/B1/ped_models/openpose.tar.gz
 tar zxvf openpose.tar.gz
 cd ./openpose
+cd ./3rdparty
+git clone https://github.com/CMU-Perceptual-Computing-Lab/caffe.git
+cd ..
 mkdir build
 cd ./build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DDOWNLOAD_BODY_25_MODEL=OFF -DDOWNLOAD_FACE_MODEL=OFF -DDOWNLOAD_HAND_MODEL=OFF ..
