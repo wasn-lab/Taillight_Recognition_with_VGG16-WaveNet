ae28d6fe54ce2af695e7a8c5bb2eb1891117cb53
Ching-Hao Liu <chinghao.liu@itri.org.tw>
2020-07-13 15:01:14 +0800

    Add pose.orientation & vector3d_to_quaternion().

--- src/sensing/obj_msg_converter/scripts/convert.py
+++ src/sensing/obj_msg_converter/scripts/convert.py
@@ -2,17 +2,43 @@
 
 from __future__ import division
 import rospy
-from geometry_msgs.msg import Polygon, PolygonStamped, Point32, Point
-from std_msgs.msg import Header
+from geometry_msgs.msg import Point32, Quaternion
 from msgs.msg import *
 from autoware_perception_msgs.msg import *
-import numpy as np
+from numpy import array_equal, negative, cross, dot
+import math
 
 
 def my_divide(dividend, divisor):
     remain = dividend % divisor
     floor = dividend // divisor
     return floor, remain
+
+
+def vector3d_to_quaternion(p):
+    u = [1, 0, 0]
+    norm1 = math.sqrt(p[0] * p[0] + p[1] * p[1] + p[2] * p[2])
+    if norm1 == 0:
+        norm1 = 1
+    v = [p[0] / norm1, p[1] / norm1, p[2] / norm1]
+
+    q = [0, 0, 0, 0]  # x, y, z, w
+    if (array_equal(u, v)):
+        q = [0, 0, 0, 1]
+    elif (array_equal(u, negative(v))):
+        q = [0, 0, 1, 0]
+    else:
+        half = [0, 0, 0]
+        half = [u[0] + v[0], u[1] + v[1], u[2] + v[2]]
+        temp = cross(u, half)
+        q = [temp[0], temp[1], temp[2], dot(u, half)]
+
+    norm2 = math.sqrt(q[0] * q[0] + q[1] * q[1] + q[2] * q[2] + q[3] * q[3])
+    if norm2 == 0:
+        norm2 = 1
+    q = [q[0] / norm2, q[1] / norm2, q[2] / norm2, q[3] / norm2]
+
+    return q
 
 
 class Node:
@@ -33,7 +59,7 @@ class Node:
     def track3d_callback(self, msg):
         self.track3d_aw_pub.publish(self.msg_convert(msg))
 
-    def classid_convert(self, input):
+    def classid_convert(self, idx):
         dict = {
             0: 0,
             1: 6,
@@ -46,7 +72,7 @@ class Node:
             8: -1,
             9: -1
         }
-        return dict.get(input)
+        return dict.get(idx)
 
     def msg_convert(self, in_list):
         out_list = DynamicObjectArray()
@@ -77,21 +103,23 @@ class Node:
                 in_obj.bPoint.p0.y + in_obj.bPoint.p7.y) / 2
             out_obj.state.pose_covariance.pose.position.z = (
                 in_obj.bPoint.p0.z + in_obj.bPoint.p7.z) / 2
-            # out_obj.state.pose_covariance.pose.orientation =
-            # out_obj.state.pose_covariance.covariance =
 
             out_obj.state.twist_covariance.twist.linear.x = in_obj.track.absolute_velocity.x
             out_obj.state.twist_covariance.twist.linear.y = in_obj.track.absolute_velocity.y
             out_obj.state.twist_covariance.twist.linear.z = in_obj.track.absolute_velocity.z
-            # out_obj.state.twist_covariance.twist.angular =
-            # out_obj.state.twist_covariance.covariance =
 
-            # out_obj.state.accel_covariance.accel.linear =
-            # out_obj.state.accel_covariance.accel.angular =
-            # out_obj.state.accel_covariance.covariance =
+            vec3 = [
+                out_obj.state.twist_covariance.twist.linear.x,
+                out_obj.state.twist_covariance.twist.linear.y,
+                out_obj.state.twist_covariance.twist.linear.z]
+
+            q = vector3d_to_quaternion(vec3)
+            out_obj.state.pose_covariance.pose.orientation = Quaternion(
+                q[0], q[1], q[2], q[3])
+            out_obj.state.orientation_reliable = True
 
             out_obj.shape.type = 2
-            # out_obj.shape.dimensions =
+
             if not in_obj.cPoint.lowerAreaPoints:
                 for p in in_obj.cPoint.lowerAreaPoints:
                     out_obj.shape.footprint.append(Point32(p.x, p.y, p.z))
