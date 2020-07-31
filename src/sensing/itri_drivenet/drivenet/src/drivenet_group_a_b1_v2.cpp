6123903f5830f87f1d58305c096e7eb6d2782b92
Mike Lin <mike19931010@gmail.com>
2020-07-31 10:41:20 +0800

    Add ft30

--- src/sensing/itri_drivenet/drivenet/src/drivenet_group_a_b1_v2.cpp
+++ src/sensing/itri_drivenet/drivenet/src/drivenet_group_a_b1_v2.cpp
@@ -357,8 +357,10 @@ msgs::DetectedObject run_dist(ITRI_Bbox box, int cam_order)
   }
   else if (g_cam_ids[cam_order] == camera::id::front_top_far_30)
   {
-    l_check = g_dist_est.CheckPointInArea(g_dist_est.area[camera::id::front_top_far_30], box.x1, box.y2);
-    r_check = g_dist_est.CheckPointInArea(g_dist_est.area[camera::id::front_top_far_30], box.x2, box.y2);
+    // l_check = g_dist_est.CheckPointInArea(g_dist_est.area[camera::id::front_top_far_30], box.x1, box.y2);
+    // r_check = g_dist_est.CheckPointInArea(g_dist_est.area[camera::id::front_top_far_30], box.x2, box.y2);
+    l_check = 0;
+    r_check = 0;
   }
 
   if (l_check == 0 && r_check == 0)
