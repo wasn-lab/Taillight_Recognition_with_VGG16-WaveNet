32f0e47ed9391f7e63ed526728bbf84847f46291
ashain6840 <ashain6840@gmail.com>
2020-08-24 09:11:03 +0800

    add real time mission input

--- src/planning/mission_input/src/mission_input.cpp
+++ src/planning/mission_input/src/mission_input.cpp
@@ -5,9 +5,13 @@
 #include <fstream>
 #include <ros/package.h>
 #include <cmath>
+#include "std_msgs/Int32.h"
+#include "msgs/Flag_Info.h"
 
 ros::Publisher goal_publisher;
 ros::Publisher checkpoint_publisher;
+
+int ORGS = 0;
 
 double seg_x[2000] = {};
 double seg_y[2000] = {};
@@ -17,6 +21,8 @@ double ori_y[2000] = {};
 double ori_z[2000] = {};
 double ori_w[2000] = {};
 int read_index = 0;
+
+int Dspace_Flag_last[8] = {0,0,0,0,0,0,0,0};
 
 template <int size_readtmp>
 void read_txt(std::string fpname, double (&SEG_X)[size_readtmp],double (&SEG_Y)[size_readtmp],
@@ -67,6 +73,14 @@ void Ini_obs_bytxt()
   read_txt(fpname_s, seg_x, seg_y, seg_z, ori_x, ori_y, ori_z, ori_w);
 }
 
+void Ini_busstop_bytxt()
+{
+  std::string fpname = ros::package::getPath("mission_input");
+  std::string fpname_s = fpname + "/data/ITRI_busstop.txt"; // full route
+
+  read_txt(fpname_s, seg_x, seg_y, seg_z, ori_x, ori_y, ori_z, ori_w);
+}
+
 void get_goal_point()
 {
   geometry_msgs::PoseStamped input_goal;
@@ -85,7 +99,7 @@ void get_goal_point()
   goal_publisher.publish(input_goal);
   std::cout << input_goal.pose.position.x << std::endl;
 
-  ros::Rate loop_rate(1);
+  ros::Rate loop_rate(0.5);
   ros::spinOnce();
   loop_rate.sleep();
 }
@@ -108,24 +122,105 @@ void get_checkpoint_point()
 
     checkpoint_publisher.publish(input_checkpoint);
     std::cout << i << std::endl;
-    ros::Rate loop_rate(1);
+    ros::Rate loop_rate(0.5);
     ros::spinOnce();
     loop_rate.sleep();
   }
 }
 
+void get_realtime_goal_point(int i)
+{
+  geometry_msgs::PoseStamped input_goal;
+
+  input_goal.header.frame_id = "map";
+  input_goal.header.stamp = ros::Time::now();
+
+  input_goal.pose.position.x = seg_x[i];
+  input_goal.pose.position.y = seg_y[i];
+  input_goal.pose.position.z = seg_z[i];
+  input_goal.pose.orientation.x = ori_x[i];
+  input_goal.pose.orientation.y = ori_y[i];
+  input_goal.pose.orientation.z = ori_z[i];
+  input_goal.pose.orientation.w = ori_w[i];
+
+  goal_publisher.publish(input_goal);
+  std::cout << input_goal.pose.position.x << std::endl;
+
+  ros::Rate loop_rate(0.5);
+  loop_rate.sleep();
+}
+
+void get_realtime_checkpoint_point(int i)
+{
+  geometry_msgs::PoseStamped input_checkpoint;
+ 
+  input_checkpoint.header.frame_id = "map";
+  input_checkpoint.header.stamp = ros::Time::now();
+  input_checkpoint.pose.position.x = seg_x[i];
+  input_checkpoint.pose.position.y = seg_y[i];
+  input_checkpoint.pose.position.z = seg_z[i];
+  input_checkpoint.pose.orientation.x = ori_x[i];
+  input_checkpoint.pose.orientation.y = ori_y[i];
+  input_checkpoint.pose.orientation.z = ori_z[i];
+  input_checkpoint.pose.orientation.w = ori_w[i];
+
+  checkpoint_publisher.publish(input_checkpoint);
+  std::cout << i << std::endl;
+  ros::Rate loop_rate(0.5);
+  loop_rate.sleep();
+}
+
 void offline_realtime_goal_setting()
 {
-  // if (ORGS == 0)
-  // {
+  if (ORGS == 0)
+  {
     Ini_obs_bytxt();
     get_goal_point();
     get_checkpoint_point();
-  // }
-  // else
-  // {
-    
-  // } 
+  }
+  else
+  {
+    Ini_busstop_bytxt();
+    ros::spin();
+  } 
+}
+
+void busstopinfoCallback(const msgs::Flag_Info::ConstPtr& msg)
+{
+  if (ORGS == 1)
+  {
+    int sum = msg->Dspace_Flag01 + msg->Dspace_Flag02 + msg->Dspace_Flag03 +	msg->Dspace_Flag04 + msg->Dspace_Flag05 +	msg->Dspace_Flag06 + msg->Dspace_Flag07 +	msg->Dspace_Flag08;
+    int Dspace_Flag[8] = {msg->Dspace_Flag01,msg->Dspace_Flag02,msg->Dspace_Flag03,msg->Dspace_Flag04,msg->Dspace_Flag05,msg->Dspace_Flag06,msg->Dspace_Flag07,msg->Dspace_Flag08};
+    for (int i = 0; i < 8; i++)
+    {
+      if (Dspace_Flag[i] != Dspace_Flag_last[i])
+      {
+        std::cout << "Dspace_Flag(3) : " << Dspace_Flag[3] << std::endl;
+        std::cout << "sum : " << sum << std::endl;
+        double bustop_id[sum] = {};
+        int index = 0;
+        for (int i = 0; i < 8; i++)
+        {
+          if (Dspace_Flag[i] == 1)
+          {
+            bustop_id[index] = i;
+            index = index + 1;
+          }
+        }
+        get_realtime_goal_point(bustop_id[sum-1]);
+        for (int i = 0; i < sum-1; i++)
+        {
+          get_realtime_checkpoint_point(bustop_id[i]);
+        }
+        int round_count = msg->PX2_Flag01;
+        for (int j = 0; j < 8; j++)
+        {
+          Dspace_Flag_last[j] = Dspace_Flag[j];
+        }
+        break;
+      }
+    }
+  }
 }
 
 int main(int argc, char** argv)
@@ -133,6 +228,8 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "mission_input");
   ros::NodeHandle node;
 
+  ros::param::get(ros::this_node::getName()+"/ORGS", ORGS);
+  ros::Subscriber busstopinfo = node.subscribe("/BusStop/Info", 1, busstopinfoCallback);
   goal_publisher = node.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 10, true);
   checkpoint_publisher = node.advertise<geometry_msgs::PoseStamped>("/checkpoint", 10, true);
 
