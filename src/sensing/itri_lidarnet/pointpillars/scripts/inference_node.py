b4bc04b7a2b407755477a9e28d590df7fba9efbe
A80131 <bshuang@itri.org.tw>
2020-08-28 14:41:23 +0800

    add pointpillars

---
+++ src/sensing/itri_lidarnet/pointpillars/scripts/inference_node.py
@@ -0,0 +1,247 @@
+import rospy
+from msgs.msg import (DetectedObject, DetectedObjectArray, PointXYZ)
+from visualization_msgs.msg import (MarkerArray, Marker)
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import PointCloud2
+from std_msgs.msg import Header
+
+import io as sysio
+import json
+import os
+import pickle
+import sys
+import time
+from functools import partial
+from pathlib import Path
+import fire
+import matplotlib.pyplot as plt
+import numba
+import numpy as np
+
+import second.core.box_np_ops as box_np_ops
+import second.core.preprocess as prep
+from second.core.anchor_generator import AnchorGeneratorStride
+from second.core.box_coders import GroundBox3dCoder
+from second.core.point_cloud.point_cloud_ops import points_to_voxel
+from second.core.region_similarity import (
+    DistanceSimilarity, NearestIouSimilarity, RotateIouSimilarity)
+from second.core.sample_ops import DataBaseSamplerV2
+from second.core.target_assigner import TargetAssigner
+from second.data import kitti_common as kitti
+from second.kittiviewer.glwidget import KittiGLViewWidget
+from second.protos import pipeline_pb2
+from second.utils import bbox_plot
+from second.utils.bbox_plot import GLColor
+from second.utils.eval import get_coco_eval_result, get_official_eval_result
+from second.pytorch.inference import TorchInferenceContext
+from second.utils.progress_bar import list_bar
+
+result_pub = rospy.Publisher ('/LidarDetection_PointPillars', DetectedObjectArray, queue_size=30)
+marker_pub = rospy.Publisher('/LidarDetection_PointPillars/bbox', MarkerArray, queue_size=10)
+
+def corners_lidar(dims, origin=[0.5, 0.5, 0.0]):
+    ndim = int(dims.shape[1])
+    corners_norm = np.stack(
+        np.unravel_index(np.arange(2**ndim), [2] * ndim), axis=1).astype(
+            dims.dtype)
+    # (3d) x0y0z0, x0y0z1, x0y1z0, x0y1z1, x1y0z0, x1y0z1, x1y1z0, x1y1z1
+    # (converted 3d) x0y0z0, x0y0z1, x0y1z1, x0y1z0, x1y0z0, x1y0z1, x1y1z1, x1y1z0
+    corners_norm = corners_norm[[0, 1, 3, 2, 4, 5, 7, 6]]
+    # translate according to origin
+    corners_norm = corners_norm - np.array(origin, dtype=dims.dtype)
+    corners = dims.reshape([-1, 1, ndim]) * corners_norm.reshape(
+        [1, 2**ndim, ndim])
+    return corners
+
+def rotation_3d_in_axis(points, angles, axis=2):
+    # points: [N, 8, 3]
+    rot_sin = np.sin(np.pi/2-angles)
+    rot_cos = np.cos(np.pi/2-angles)
+    ones = np.ones_like(rot_cos)
+    zeros = np.zeros_like(rot_cos)
+    if axis == 1:
+        rot_mat_T = np.stack([[rot_cos, zeros, -rot_sin], [zeros, ones, zeros],
+                              [rot_sin, zeros, rot_cos]])
+    elif axis == 2 or axis == -1:
+        rot_mat_T = np.stack([[rot_cos, -rot_sin, zeros],
+                              [rot_sin, rot_cos, zeros], [zeros, zeros, ones]])
+    elif axis == 0:
+        rot_mat_T = np.stack([[zeros, rot_cos, -rot_sin],
+                              [zeros, rot_sin, rot_cos], [ones, zeros, zeros]])
+    else:
+        raise ValueError("axis should in range")
+    
+    # result: [N, 8, 3]
+    return np.einsum('aij,jka->aik', points, rot_mat_T)
+
+def center_to_corner_lidar(centers,
+                            dims,
+                            angles=None,
+                            origin=[0.5, 0.5, 0],
+                            axis=2):
+    corners = corners_lidar(dims, origin=origin)
+    if angles is not None:
+        corners = rotation_3d_in_axis(corners, angles, axis=axis)
+    corners += centers.reshape([-1, 1, 3])
+    # corners: [N, 8, 3]
+    return corners
+
+class Inference_node:
+    def __init__(self):
+        self.inference_ctx = None
+        self.points = None
+    
+    def build(self, config_path_str):
+        self.inference_ctx = TorchInferenceContext()
+        vconfig_path = Path(config_path_str)
+        self.inference_ctx.build(vconfig_path)
+        rospy.loginfo("[pointpillars] Build PointPillars succeed.")
+    
+    def  loadckpt(self, ckpt_path_str):
+        ckpt_path = Path(ckpt_path_str)
+        self.inference_ctx.restore(ckpt_path)
+        rospy.loginfo("[pointpillars] load PointPillars ckpt succeed.")
+
+    def callback_lidarAll(self, msg):
+        # This is where pp inference code put in 
+        
+        # first read point cloud array from message
+        #dtype_list = [(f.name, np.float32) for f in msg.fields]
+        dtype_list = [('x', np.float32), ('y', np.float32), ('z', np.float32), ('blank', np.float32), ('intensity', np.float32),
+                                    ('blank2', np.float32), ('blank3', np.float32), ('blank4', np.float32)]
+        points = np.fromstring(msg.data, dtype_list)
+        points = np.array(points.tolist())[:, [0, 1, 2, 4]]
+        # intensity_max = np.max(points[:,3])
+        # print ("max: ", intensity_max)
+        # points[:,3] = points[:,3] * 0
+        points[:,3] = points[:,3]  * 255/1500
+        np.reshape(points, (msg.height, msg.width, -1))
+        lidar_time = msg.header.stamp.secs
+        #stamp = msg.header.stamp
+        # check input feature size and feature name
+        
+        assert dtype_list[4][0] == "intensity" or dtype_list[4][0] == "reflection"
+        # convert input into preprocessed example
+        t = time.time()
+        inputs = self.inference_ctx.get_inference_input_dict_without_kitti(lidar_time, points)
+        rospy.loginfo("[pointpillars] input preparation time: %fs", time.time()-t)
+        t = time.time()
+        with self.inference_ctx.ctx():
+            det_annos = self.inference_ctx.inference_lidar(inputs)
+        rospy.loginfo("[pointpillars] detection time: %fs", time.time()-t)
+        num_anno = det_annos[0]["name"].shape[0]
+        anno = det_annos[0]
+        #print(det_annos)
+
+        det_object_array = DetectedObjectArray()
+        det_marker_array = MarkerArray()
+        id = 0
+
+        for i in range(num_anno):
+            det_object = DetectedObject()
+            det_object.header = msg.header
+            classID = anno["name"][i]
+            if classID == "Car":
+                det_object.classId = 4
+            elif classID == "Pedestrian":
+                det_object.classId = 1
+            elif classID == "Cyclist":
+                det_object.classId = 3
+            # center is in [N, 3]
+            bCenter = np.array(anno["location"][i]).reshape([-1, 3])
+            # bCenter[:, 0] = -bCenter[:,0]
+            # print(bCenter)
+            bDims = np.array([anno["dimensions"][i]]).reshape([-1, 3])
+            bRotation = np.array([anno["rotation_y"][i]]).reshape([-1])
+            #bCorners: [N, 8, 3]
+            bCorners = center_to_corner_lidar(bCenter, bDims[:,[1, 0, 2]], bRotation, axis=2)
+            det_object.lidarInfo.boxCenter.x = bCenter[0][0]
+            det_object.lidarInfo.boxCenter.y = bCenter[0][1]
+            det_object.lidarInfo.boxCenter.z = bCenter[0][2]
+            # print(bDims.shape)
+            # det_object.lidarInfo.width = bDims[0][1]
+            # det_object.lidarInfo.height = bDims[0][2]
+            det_object.bPoint.p0 = PointXYZ(bCorners[0][0][0], bCorners[0][0][1], bCorners[0][0][2])
+            det_object.bPoint.p1 = PointXYZ(bCorners[0][1][0], bCorners[0][1][1], bCorners[0][1][2])
+            det_object.bPoint.p2 = PointXYZ(bCorners[0][2][0], bCorners[0][2][1], bCorners[0][2][2])
+            det_object.bPoint.p3 = PointXYZ(bCorners[0][3][0], bCorners[0][3][1], bCorners[0][3][2])
+            det_object.bPoint.p4 = PointXYZ(bCorners[0][4][0], bCorners[0][4][1], bCorners[0][4][2])
+            det_object.bPoint.p5 = PointXYZ(bCorners[0][5][0], bCorners[0][5][1], bCorners[0][5][2])
+            det_object.bPoint.p6 = PointXYZ(bCorners[0][6][0], bCorners[0][6][1], bCorners[0][6][2])
+            det_object.bPoint.p7 = PointXYZ(bCorners[0][7][0], bCorners[0][7][1], bCorners[0][7][2])
+            rot_sin = np.sin(bRotation[0]/2)
+            rot_cos = np.cos(bRotation[0]/2)
+            det_object.bOrient.w,det_object.bOrient.x, det_object.bOrient.y, det_object.bOrient.z = rot_cos, 0.0, 0.0, rot_sin
+            det_object.header = msg.header
+            det_object.header.frame_id = "lidar"
+            det_object_array.objects.append(det_object)
+
+            if (np.all(bCenter[0] > np.array([-69.12, -39.68, -4]))) and (np.all(bCenter[0] <np.array([69.12, 39.68, 0]))) and (np.all(bDims[0] < np.array([10.0, 10.0, 10.0]))) and (anno["score"][i] > 0.0):
+                det_marker = Marker()
+                det_marker.type = Marker.LINE_LIST
+                det_marker.header = msg.header
+                det_marker.header.frame_id = "lidar"
+                det_marker.id = id
+                det_marker.lifetime = rospy.Duration(0.2)
+                # det_marker.scale.x, det_marker.scale.y, det_marker.scale.z = bDims[0][0], bDims[0][1], bDims[0][2]
+                det_marker.scale.x = 0.1
+                # det_marker.pose.position.x, det_marker.pose.position.y, det_marker.pose.position.z = bCenter[0][0], bCenter[0][1], bCenter[0][2]
+                det_marker.pose.orientation.w, det_marker.pose.orientation.x, det_marker.pose.orientation.y, det_marker.pose.orientation.z = 1.0, 0.0, 0.0, 0.0
+                for i in range(4):
+                    p, p_next = Point(), Point()
+                    p.x, p.y, p.z = bCorners[0][i][0], bCorners[0][i][1], bCorners[0][i][2]
+                    if i != 3:
+                        p_next.x, p_next.y, p_next.z = bCorners[0][i+1][0], bCorners[0][i+1][1], bCorners[0][i+1][2]
+                    else:
+                        p_next.x, p_next.y, p_next.z = bCorners[0][0][0], bCorners[0][0][1], bCorners[0][0][2]
+                    det_marker.points.append(p)
+                    det_marker.points.append(p_next)
+                for i in range(4, 8):
+                    p, p_next = Point(), Point()
+                    p.x, p.y, p.z = bCorners[0][i][0], bCorners[0][i][1], bCorners[0][i][2]
+                    if i != 7:
+                        p_next.x, p_next.y, p_next.z = bCorners[0][i+1][0], bCorners[0][i+1][1], bCorners[0][i+1][2]
+                    else:
+                        p_next.x, p_next.y, p_next.z = bCorners[0][4][0], bCorners[0][4][1], bCorners[0][4][2]
+                    det_marker.points.append(p)
+                    det_marker.points.append(p_next)
+                for i in range(4):
+                    p, p_next = Point(), Point()
+                    p.x, p.y, p.z = bCorners[0][i][0], bCorners[0][i][1], bCorners[0][i][2]
+                    p_next.x, p_next.y, p_next.z = bCorners[0][i+4][0], bCorners[0][i+4][1], bCorners[0][i+4][2]
+                    det_marker.points.append(p)
+                    det_marker.points.append(p_next)
+                if det_object.classId == 4:
+                    det_marker.color.a, det_marker.color.r, det_marker.color.g, det_marker.color.b = 1.0, 0.0, 1.0, 0.0
+                elif det_object.classId == 1:
+                    det_marker.color.a, det_marker.color.r, det_marker.color.g, det_marker.color.b = 1.0, 0.0, 1.0, 1.0
+                elif det_object.classId == 3:
+                    det_marker.color.a, det_marker.color.r, det_marker.color.g, det_marker.color.b = 1.0, 1.0, 0.0, 1.0
+                det_marker_array.markers.append(det_marker)
+                id += 1
+
+        result_pub.publish(det_object_array)
+        if det_marker_array.markers is not None:
+            marker_pub.publish(det_marker_array)
+
+def main():
+    rospy.init_node('pointpillars_inference')
+    rospy.loginfo("[pointpillars] inference node running")
+    
+    # Inference node object
+    infer_node = Inference_node()
+    # infer_node.build("./second/configs/pointpillars/car/xyres_16.proto")
+    # infer_node.loadckpt("./second/model_20200722/eval_checkpoints/voxelnet-296960.tckpt")
+    infer_node.build("./second/configs/pointpillars/ped_cycle/xyres_16.proto")
+    infer_node.loadckpt("./second/model_ped_cycle_20200803/eval_checkpoints/voxelnet-294556.tckpt")
+    
+    rospy.Subscriber("/LidarAll/NonGround", PointCloud2, infer_node.callback_lidarAll)
+
+    rospy.spin()
+        
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
--- src/utilities/data_analysis/Batch_data_analysis.py
+++ src/utilities/data_analysis/Batch_data_analysis.py
@@ -14,37 +14,47 @@ except ImportError:
 def input_parser(inputpath):
     objectArray = []
     tree = ET.ElementTree(file=inputpath)
-    for elem in tree.iterfind('lidar/object'):
-        id = int(elem[0].text)  # ID
-        objectType = elem[1].text   # type
-        xCenter = float(elem.find('pcd_bndbox/x_center').text) # pcd_bndbox/x_center
-        yCenter = float(elem.find('pcd_bndbox/y_center').text) # pcd_bndbox/y_center
-        zCenter = float(elem.find('pcd_bndbox/z_center').text) # pcd_bndbox/z_center
-        objectArray.append([id, objectType, xCenter, yCenter, zCenter])
-    return objectArray
+    if tree.find("lidar/frame_shadow").text != "1":
+        for elem in tree.iterfind('lidar/object'):
+            id = int(elem[0].text)  # ID
+            objectType = elem[1].text   # type
+            xCenter = float(elem.find('pcd_bndbox/x_center').text) # pcd_bndbox/x_center
+            yCenter = float(elem.find('pcd_bndbox/y_center').text) # pcd_bndbox/y_center
+            zCenter = float(elem.find('pcd_bndbox/z_center').text) # pcd_bndbox/z_center
+            objectArray.append([id, objectType, xCenter, yCenter, zCenter])
+        return objectArray
+    else:
+        print("Frame has shadow")
+        return None
 
 def object_counter(objectArray):
     # count variable
     # car, ped, bike, front, 
     # front_right, front_left, right, left,
     # rear_right, rear_left, rear
-    cnt_list = [0 for x in range(14)]
-    cnt_detail_list = [0 for x in range(72)]
+    cnt_list = [0 for x in range(16)]
+    cnt_detail_list = [0 for x in range(120)]
     count = 0
     a = 0
     b = 0
     c = 0
     for item in objectArray:
         count = count + 1
-        if item[1] == "Car" or item[1] == "Bus":
+        if item[1] == "Car":
             cnt_list[0] = cnt_list[0] + 1
             b = 0
         elif item[1] == "Pedestrian":
             cnt_list[1] = cnt_list[1] + 1
             b = 1
-        elif item[1] == "Bicycle" or item[1] == "Motorcycle":
+        elif item[1] == "Bicycle" or item[1] == "Motorcycle" or item[1] == "Motorbike":
             cnt_list[2] = cnt_list[2] + 1
             b = 2
+        elif  item[1] == "Bus":
+            cnt_list[3] = cnt_list[3] + 1
+            b = 3
+        elif item[1] == "Truck":
+            cnt_list[4] = cnt_list[4] +1
+            b = 4
         else:
             print (item[1])
         
@@ -53,60 +63,60 @@ def object_counter(objectArray):
             if abs(item[3]/item[2]) <= float(1)/float(1+math.sqrt(2)):
                 # front
                 if item[2] > 0:
-                    cnt_list[3] = cnt_list[3] + 1
+                    cnt_list[5] = cnt_list[5] + 1
                     c = 0
                 # rear
                 else:
-                    cnt_list[-1] = cnt_list[-1] + 1
+                    cnt_list[12] = cnt_list[12] + 1
                     c = 7
             # right or left
             elif abs(item[3]/item[2]) >= float(1+math.sqrt(2)):
                 # right
                 if item[3] < 0:
-                    cnt_list[6] = cnt_list[6] + 1
+                    cnt_list[8] = cnt_list[8] + 1
                     c = 3
                 # left
                 else:
-                    cnt_list[7] = cnt_list[7] + 1
+                    cnt_list[9] = cnt_list[9] + 1
                     c = 4
             # others
             else:
                 # front_right
                 if item[2] > 0 and item[3] < 0:
-                    cnt_list[4] = cnt_list[4] + 1
+                    cnt_list[6] = cnt_list[6] + 1
                     c = 1
                 # front_left
                 elif item[2] > 0 and item[3] > 0:
-                    cnt_list[5] = cnt_list[5] + 1
+                    cnt_list[7] = cnt_list[7] + 1
                     c = 2
                 # rear_right
                 elif item[2] < 0 and item[3] < 0:
-                    cnt_list[8] = cnt_list[8] + 1
+                    cnt_list[10] = cnt_list[10] + 1
                     c = 5
                 # rear_left
                 elif item[2] < 0 and item[3] > 0:
-                    cnt_list[9] = cnt_list[9] + 1
+                    cnt_list[11] = cnt_list[11] + 1
                     c = 6
         else:
             if item[3] < 0:
-                cnt_list[6] = cnt_list[6] + 1
+                cnt_list[8] = cnt_list[8] + 1
                 c = 3
             elif item[3] > 0:
-                cnt_list[7] = cnt_list[7] + 1
+                cnt_list[9] = cnt_list[9] + 1
                 c = 4
         
         dist_square = math.pow(item[2], 2) + math.pow(item[3], 2)
         if dist_square <= 225:
-            cnt_list[11] = cnt_list[11] + 1
+            cnt_list[13] = cnt_list[13] + 1
             a = 0
         elif dist_square > 900:
-            cnt_list[13] = cnt_list[13] + 1
+            cnt_list[15] = cnt_list[15] + 1
             a = 2
         else:
-            cnt_list[12] = cnt_list[12] + 1
+            cnt_list[14] = cnt_list[14] + 1
             a = 1
         
-        cnt_detail_list[a*24+b*8+c] = cnt_detail_list[a*24+b*8+c] + 1
+        cnt_detail_list[a*40+b*8+c] = cnt_detail_list[a*40+b*8+c] + 1
         
     return cnt_list, cnt_detail_list, count
 
@@ -130,8 +140,8 @@ def main():
                     print ("File in Directory: %s" % current_dir)
                     if os.path.isdir(current_dir) and not (dir_name.startswith(".")):
                         xml_list = sorted(os.listdir(os.getcwd()))
-                        dir_obj_cnts = [0 for x in range(14)]
-                        dir_obj_detail_cnts = [0 for x in range(72)]
+                        dir_obj_cnts = [0 for x in range(16)]
+                        dir_obj_detail_cnts = [0 for x in range(120)]
                         total_cnts = 0
                         for xml_file in xml_list:
                             if xml_file.endswith(".xml"):
@@ -141,44 +151,48 @@ def main():
 
                                 xml_path = current_dir + "/" + xml_file
                                 xml_objs = input_parser(xml_path)
-                                obj_cnts, obj_detail_cnts, file_cnts = object_counter(xml_objs)
-                                dir_obj_cnts = [dir_obj_cnts[i] + obj_cnts[i] for i in range(len(obj_cnts))]
-                                dir_obj_detail_cnts = [dir_obj_detail_cnts[i] + obj_detail_cnts[i] for i in range(len(obj_detail_cnts))]
-                                total_cnts = total_cnts + file_cnts
+                                if xml_objs is not None:
+                                    obj_cnts, obj_detail_cnts, file_cnts = object_counter(xml_objs)
+                                    dir_obj_cnts = [dir_obj_cnts[i] + obj_cnts[i] for i in range(len(obj_cnts))]
+                                    dir_obj_detail_cnts = [dir_obj_detail_cnts[i] + obj_detail_cnts[i] for i in range(len(obj_detail_cnts))]
+                                    total_cnts = total_cnts + file_cnts
                         # print statistics
                         print ("** Statistics of %s **" % dir_name )
                         print ("Total Objects: %d" % total_cnts)
                         print ("Classified by Type:")
                         print ("    Objects of Type Car: %d" % dir_obj_cnts[0])
-                        print ("    Objects of Type Person: %d" % dir_obj_cnts[1])
-                        print ("    Objects of Type Motobike: %d" % dir_obj_cnts[2])
+                        print ("    Objects of Type Pedestrian: %d" % dir_obj_cnts[1])
+                        print ("    Objects of Type Cyclist: %d" % dir_obj_cnts[2])
+                        print ("    Objects of Type Bus: %d" % dir_obj_cnts[3])
+                        print ("    Objects of Type Truck: %d" % dir_obj_cnts[4])
                         print ("Classified by direction:")
-                        print ("    Objects of in front: %d" % dir_obj_cnts[3])
-                        print ("    Objects of in front right: %d" % dir_obj_cnts[4])
-                        print ("    Objects of in front left: %d" % dir_obj_cnts[5])
-                        print ("    Objects of in right: %d" % dir_obj_cnts[6])
-                        print ("    Objects of in left: %d" % dir_obj_cnts[7])
-                        print ("    Objects of in rear right: %d" % dir_obj_cnts[8])
-                        print ("    Objects of in rear left: %d" % dir_obj_cnts[9])
-                        print ("    Objects of in rear: %d" % dir_obj_cnts[10])
+                        print ("    Objects of in front: %d" % dir_obj_cnts[5])
+                        print ("    Objects of in front right: %d" % dir_obj_cnts[6])
+                        print ("    Objects of in front left: %d" % dir_obj_cnts[7])
+                        print ("    Objects of in right: %d" % dir_obj_cnts[8])
+                        print ("    Objects of in left: %d" % dir_obj_cnts[9])
+                        print ("    Objects of in rear right: %d" % dir_obj_cnts[10])
+                        print ("    Objects of in rear left: %d" % dir_obj_cnts[11])
+                        print ("    Objects of in rear: %d" % dir_obj_cnts[12])
                         print ("Classified by range:")
-                        print ("    Objects in 15m: %d" % dir_obj_cnts[11])
-                        print ("    Objects between 15m to 30m: %d" % dir_obj_cnts[12])
-                        print ("    Objects over 30m: %d" % dir_obj_cnts[13])
+                        print ("    Objects in 15m: %d" % dir_obj_cnts[13])
+                        print ("    Objects between 15m to 30m: %d" % dir_obj_cnts[14])
+                        print ("    Objects over 30m: %d" % dir_obj_cnts[15])
 
                         print ("\n** Details of %s **" % dir_name)
-                        headers = ["Car", "Person", "Motobike"]
+                        headers = ["Car", "Pedestrian", "Cyclist", "Bus", "Truck"]
+                        table_elem_num = 40
                         table = []
                         for i in range(3):
-                            table.append([["front", dir_obj_detail_cnts[0+i*24], dir_obj_detail_cnts[8+i*24], dir_obj_detail_cnts[16+i*24]],
-                                                        ["front_right", dir_obj_detail_cnts[1+i*24], dir_obj_detail_cnts[9+i*24], dir_obj_detail_cnts[17+i*24]],
-                                                        ["front_left", dir_obj_detail_cnts[2+i*24], dir_obj_detail_cnts[10+i*24], dir_obj_detail_cnts[18+i*24]],
-                                                        ["right", dir_obj_detail_cnts[3+i*24], dir_obj_detail_cnts[11+i*24], dir_obj_detail_cnts[19+i*24]],
-                                                        ["left", dir_obj_detail_cnts[4+i*24], dir_obj_detail_cnts[12+i*24], dir_obj_detail_cnts[20+i*24]],
-                                                        ["rear_right", dir_obj_detail_cnts[5+i*24], dir_obj_detail_cnts[13+i*24], dir_obj_detail_cnts[21+i*24]],
-                                                        ["rear_left", dir_obj_detail_cnts[6+i*24], dir_obj_detail_cnts[14+i*24], dir_obj_detail_cnts[22+i*24]],
-                                                        ["rear", dir_obj_detail_cnts[7+i*24], dir_obj_detail_cnts[15+i*24], dir_obj_detail_cnts[23+i*24]]])
-                            print (tabulate(table[i], headers, tablefmt="pretty", numalign="right", colalign=("center", "center", "center")))
+                            table.append([["front", dir_obj_detail_cnts[0+i*table_elem_num], dir_obj_detail_cnts[8+i*table_elem_num], dir_obj_detail_cnts[16+i*table_elem_num], dir_obj_detail_cnts[24+i*table_elem_num], dir_obj_detail_cnts[32+i*table_elem_num]],
+                                                        ["front_right", dir_obj_detail_cnts[1+i*table_elem_num], dir_obj_detail_cnts[9+i*table_elem_num], dir_obj_detail_cnts[17+i*table_elem_num], dir_obj_detail_cnts[25+i*table_elem_num], dir_obj_detail_cnts[33+i*table_elem_num]],
+                                                        ["front_left", dir_obj_detail_cnts[2+i*table_elem_num], dir_obj_detail_cnts[10+i*table_elem_num], dir_obj_detail_cnts[18+i*table_elem_num], dir_obj_detail_cnts[26+i*table_elem_num], dir_obj_detail_cnts[34+i*table_elem_num]],
+                                                        ["right", dir_obj_detail_cnts[3+i*table_elem_num], dir_obj_detail_cnts[11+i*table_elem_num], dir_obj_detail_cnts[19+i*table_elem_num], dir_obj_detail_cnts[27+i*table_elem_num], dir_obj_detail_cnts[35+i*table_elem_num]],
+                                                        ["left", dir_obj_detail_cnts[4+i*table_elem_num], dir_obj_detail_cnts[12+i*table_elem_num], dir_obj_detail_cnts[20+i*table_elem_num], dir_obj_detail_cnts[28+i*table_elem_num], dir_obj_detail_cnts[36+i*table_elem_num]],
+                                                        ["rear_right", dir_obj_detail_cnts[5+i*table_elem_num], dir_obj_detail_cnts[13+i*table_elem_num], dir_obj_detail_cnts[21+i*table_elem_num], dir_obj_detail_cnts[29+i*table_elem_num], dir_obj_detail_cnts[37+i*table_elem_num]],
+                                                        ["rear_left", dir_obj_detail_cnts[6+i*table_elem_num], dir_obj_detail_cnts[14+i*table_elem_num], dir_obj_detail_cnts[22+i*table_elem_num], dir_obj_detail_cnts[30+i*table_elem_num], dir_obj_detail_cnts[38+i*table_elem_num]],
+                                                        ["rear", dir_obj_detail_cnts[7+i*table_elem_num], dir_obj_detail_cnts[15+i*table_elem_num], dir_obj_detail_cnts[23+i*table_elem_num], dir_obj_detail_cnts[31+i*table_elem_num], dir_obj_detail_cnts[39+i*table_elem_num]]])
+                            print (tabulate(table[i], headers, tablefmt="pretty", numalign="right", colalign=("center", "center", "center", "center", "center")))
 
 
 
