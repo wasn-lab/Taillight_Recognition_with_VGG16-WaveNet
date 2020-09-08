70cda4d23cd7c9b55358de4b922d26306f3068af
chtseng <chtseng@itri.org.tw>
2020-09-08 13:46:55 +0800

    Revise post_to_backend.py.

--- src/utilities/alexab_darknet/drivenet_weights_mr_test/post_to_backend.py
+++ src/utilities/alexab_darknet/drivenet_weights_mr_test/post_to_backend.py
@@ -1,39 +1,25 @@
 #!/usr/bin/env python
-import requests
 import argparse
 import pprint
-import json
 import io
-import re
+import requests
 
 URL = "http://60.250.196.133:8300/DataMgmt/BackendService"
 
 
-def post_accuracy(accuracy, _id):
-    jdata = {"type": "DM.001", "accuracy": accuracy, "id": _id}
-    resp = requests.post(URL, data=json.dumps(jdata))
+def _post_to_backend(json_result):
+    with io.open(json_result, encoding="utf-8") as _fp:
+        contents = _fp.read()
+    resp = requests.post(URL, data=contents)
     pprint.pprint(resp.json())
-
-
-def parse_log(log_file):
-    with io.open(log_file, encoding="utf-8") as _fp:
-        contents = _fp.read().splitlines()
-    rgx = re.compile(r"for conf_thresh = .*, precision = (?P<accuracy>[\d\.]+), recall = .*, F1-score = .*")
-    for line in contents:
-        line = line.strip()
-        match = rgx.match(line)
-        if match:
-            return match.expand(r"\g<accuracy>")
-    return "0.87"
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--log-file", required=True)
-    parser.add_argument("--id", type=int, required=True)
+    parser.add_argument("--json-result", required=True)
     args = parser.parse_args()
-    accuracy = parse_log(args.log_file)
-    post_accuracy(accuracy, args.id)
+    _post_to_backend(args.json_result)
+
 
 if __name__ == "__main__":
     main()
