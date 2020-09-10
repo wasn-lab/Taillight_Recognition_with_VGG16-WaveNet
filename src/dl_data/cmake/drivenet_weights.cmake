cb753ef6d536f361e32810d8191b43387a418b62
ellenwang6330 <ellenwang6330@itri.org.tw>
2020-09-10 11:25:33 +0800

    update drivenet 2D camera weights of mixed FOV60/FOV120

--- src/dl_data/cmake/drivenet_weights.cmake
+++ src/dl_data/cmake/drivenet_weights.cmake
@@ -1,9 +1,9 @@
 download_file(
     URL "http://118.163.54.109:8888/git_data/B1/drivenet/yolov3_b1.weights"
     DESTINATION "${DRIVENET_WEIGHTS_DIR}/yolov3_b1.weights"
-    MD5 71c553ced70066516141b7ab8fda3654)
+    MD5 bf222d362228562b60f5300bbf9e4cb4)
 
 download_file(
     URL "http://118.163.54.109:8888/git_data/B1/drivenet/yolov3_fov120_b1.weights"
     DESTINATION "${DRIVENET_WEIGHTS_DIR}/yolov3_fov120_b1.weights"
-    MD5 4118310143beeb928b9037a8e2b32fea)
+    MD5 bf222d362228562b60f5300bbf9e4cb4)
