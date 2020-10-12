f2ea39e3ccba267e4abf3ee178408fdcceab8de2
YiLun.Chiu <YiLun.Chiu@itri.org.tw>
2020-10-13 01:54:55 +0800

    Add back prediction switch.

--- src/sensing/itri_pedcross/skip_frame_server.py
+++ src/sensing/itri_pedcross/skip_frame_server.py
@@ -7,6 +7,7 @@ from msgs.srv import *
 predict_frames = 1  # frame number for skip frame
 gain = 5  # gain for Kalman Filter
 frame_num = 10  # frame number for crossing predictionz
+do_back_prediction = True
 
 def keypoint_is_detected(keypoint):
     if keypoint.x == 0 and keypoint.y == 0:
@@ -15,7 +16,7 @@ def keypoint_is_detected(keypoint):
         return True
 
 def callback(req):
-    global predict_frames, gain
+    global predict_frames, gain, do_back_prediction
     predicted_keypoints = []
     processed_keypoints = []
 
@@ -46,23 +47,25 @@ def callback(req):
             predict_keypoints.keypoint.append(predict_keypoint)
         predicted_keypoints.append(predict_keypoints)
     
-    # calculate processed_keypoints
-    diff = Keypoints()
-    for i in range(len(latest_openpose.keypoint)):
-        diff_keypoint = Keypoint()
-        if keypoint_is_detected(latest_openpose.keypoint[i]) and keypoint_is_detected(second_latest_openpose.keypoint[i]):
-            diff_keypoint.x = (latest_openpose.keypoint[i].x - second_latest_openpose.keypoint[i].x) / (predict_frames + 1)
-            diff_keypoint.y = (latest_openpose.keypoint[i].y - second_latest_openpose.keypoint[i].y) / (predict_frames + 1)
-        else:
-            diff_keypoint.x = 0
-            diff_keypoint.y = 0
-        diff.keypoint.append(diff_keypoint)
-    for i in range(predict_frames):
-        predict_keypoints = Keypoints()
-        for j in range(len(diff.keypoint)):
-            # processed_keypoints = (original keypoints + interpolation keypoints) / 2
-            processed_keypoints[frame_num - 2 - predict_frames + (i + 1)].keypoint[j].x = (processed_keypoints[frame_num - 2 - predict_frames + (i + 1)].keypoint[j].x + second_latest_openpose.keypoint[j].x + diff.keypoint[j].x * (i + 1)) / 2
-            processed_keypoints[frame_num - 2 - predict_frames + (i + 1)].keypoint[j].y = (processed_keypoints[frame_num - 2 - predict_frames + (i + 1)].keypoint[j].y + second_latest_openpose.keypoint[j].y + diff.keypoint[j].y * (i + 1)) / 2
+    if do_back_prediction:
+        # calculate processed_keypoints
+        diff = Keypoints()
+        for i in range(len(latest_openpose.keypoint)):
+            diff_keypoint = Keypoint()
+            if keypoint_is_detected(latest_openpose.keypoint[i]) and keypoint_is_detected(second_latest_openpose.keypoint[i]):
+                diff_keypoint.x = (latest_openpose.keypoint[i].x - second_latest_openpose.keypoint[i].x) / (predict_frames + 1)
+                diff_keypoint.y = (latest_openpose.keypoint[i].y - second_latest_openpose.keypoint[i].y) / (predict_frames + 1)
+            else:
+                diff_keypoint.x = 0
+                diff_keypoint.y = 0
+            diff.keypoint.append(diff_keypoint)
+        for i in range(predict_frames):
+            predict_keypoints = Keypoints()
+            for j in range(len(diff.keypoint)):
+                # processed_keypoints = (original keypoints + interpolation keypoints) / 2
+                processed_keypoints[frame_num - 2 - predict_frames + (i + 1)].keypoint[j].x = (processed_keypoints[frame_num - 2 - predict_frames + (i + 1)].keypoint[j].x + second_latest_openpose.keypoint[j].x + diff.keypoint[j].x * (i + 1)) / 2
+                processed_keypoints[frame_num - 2 - predict_frames + (i + 1)].keypoint[j].y = (processed_keypoints[frame_num - 2 - predict_frames + (i + 1)].keypoint[j].y + second_latest_openpose.keypoint[j].y + diff.keypoint[j].y * (i + 1)) / 2
+
     return PredictSkeletonResponse(predicted_keypoints, processed_keypoints)
 
 def listener():
