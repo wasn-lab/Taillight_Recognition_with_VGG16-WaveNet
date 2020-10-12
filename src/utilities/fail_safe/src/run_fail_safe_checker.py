9e26f625d5f317ed66f5888f63aa1a7f4681fb1b
chtseng <chtseng@itri.org.tw>
2020-10-12 15:00:12 +0800

    Add ros package files.

--- src/utilities/fail_safe/src/run_fail_safe_checker.py
+++ src/utilities/fail_safe/src/run_fail_safe_checker.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python
 import argparse
+import os
+import rospkg
 from fail_safe_checker import FailSafeChecker
 
 
 def main():
+    pkg_dir = rospkg.RosPack().get_path("fail_safe")
+    src_dir = os.path.join(pkg_dir, "src")
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--vid", default="b1", help="vehicle id")
-    parser.add_argument("--ini", default="fail_safe.ini")
-    parser.add_argument("--mqtt-ini", default="mqtt_b1_v2.ini")
+    parser.add_argument("--ini", default=os.path.join(src_dir, "fail_safe.ini"))
+    parser.add_argument("--mqtt-ini", default=os.path.join(src_dir, "mqtt_b1_v2.ini"))
     parser.add_argument("--debug-mode", action="store_true")
-    args = parser.parse_args()
+    args = parser.parse_known_args()[0]
+
     checker = FailSafeChecker(args.vid, args.ini, args.mqtt_ini)
     checker.set_debug_mode(args.debug_mode)
 
