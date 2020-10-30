be72a1b98b0394c4976af91b61790a5191cf3772
chtseng <chtseng@itri.org.tw>
2020-10-30 15:07:54 +0800

    Do not use LFTP env var for easy deployment.

--- src/utilities/fail_safe/src/run_rosbag_sender.py
+++ src/utilities/fail_safe/src/run_rosbag_sender.py
@@ -22,6 +22,7 @@ def main():
     _rate = cfg["ftp"].getint("upload_rate", 1000000)
     sender = RosbagSender(cfg["ftp"]["fqdn"], cfg["ftp"]["port"],
                            cfg["ftp"]["user_name"],
+                           cfg["ftp"]["password"],
                            cfg["rosbag"]["backup_dir"],
                            vid=args.vid,
                            upload_rate=_rate)
