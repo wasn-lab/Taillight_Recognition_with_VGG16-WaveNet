44d9c304181acc4eb48d010a329742ba38e1b992
chtseng <chtseng@itri.org.tw>
2020-11-12 15:12:53 +0800

    Add vk221_3.py.

---
+++ src/utilities/fail_safe/src/vk221_3.py
@@ -0,0 +1,18 @@
+"""
+API for sending uploaded bag name to the backend server.
+"""
+import requests
+from sb_param_utils import get_license_plate_number
+from backend_info import WEBAPI_BASE_URL
+
+def build_vk221_3_url(bag, plate=None):
+    if plate is None:
+        plate = get_license_plate_number()
+    return ("{}/WebAPI?type=M8.2.VK221_3"
+            "&plate={}&bag_file={}".format(WEBAPI_BASE_URL, plate, bag))
+
+
+def notify_backend_with_new_bag(bag, plate=None):
+    url = build_vk221_3_url(bag, plate)
+    resp = requests.post(url)
+    return resp.json()
