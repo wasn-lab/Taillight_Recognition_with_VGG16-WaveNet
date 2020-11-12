4ead56a045b12631574f03017a5503fe50473d4c
chtseng <chtseng@itri.org.tw>
2020-11-12 14:18:59 +0800

    Revise run_unittest.sh.

--- src/utilities/fail_safe/src/run_unittest.sh
+++ src/utilities/fail_safe/src/run_unittest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+readonly repo_dir=$(git rev-parse --show-toplevel)
 set -e
 set -x
+cd ${repo_dir}/src/utilities/fail_safe/src
 python rosbag_utils_test.py
 python sb_param_utils_test.py
