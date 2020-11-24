bb9fb888021c68f37d98268626ab054d9bb634c5
lidaripc <lidaripc@example.com>
2020-11-24 14:10:28 +0800

    Update itriadv_run scripts on lidar_ipc.

--- src/scripts/bus_env/lidar/ip_forwarding.sh
+++ src/scripts/bus_env/lidar/ip_forwarding.sh
@@ -2,3 +2,8 @@ sudo iptables -A FORWARD -o enp8s0 -i enp1s0f1 -s 192.168.3.10/24 -m conntrack -
 sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -t nat -F POSTROUTING
 sudo iptables -t nat -A POSTROUTING -o enp8s0 -j MASQUERADE
+
+sudo iptables -A FORWARD -o enp8s0 -i enp0s31f6 -s 192.168.4.10/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t nat -F POSTROUTING
+sudo iptables -t nat -A POSTROUTING -o enp8s0 -j MASQUERADE
