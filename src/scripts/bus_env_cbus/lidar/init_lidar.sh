9a6286b1fb58fab3b1c25826d8db64cb38849f88
ashain6840 <ashain6840@gmail.com>
2020-11-26 16:12:32 +0800

    add cbus scripts

---
+++ src/scripts/bus_env_cbus/lidar/init_lidar.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+source /home/lidar/itriadv/devel/setup.bash
+export ROS_MASTER_URI=http://192.168.1.3:11311
+export ROS_IP=192.168.1.3
+roslaunch lidar b1.launch use_filter:=true use_compress:=false
+sleep 1  
+#roslaunch lidar b1.launch hardware_enable:=0
+#roslaunch sdb lidar.launch
+
