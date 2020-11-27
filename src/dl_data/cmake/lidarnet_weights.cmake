d0ae02679472d123d76d234663a63a0f36ec5a87
MikeChan <m7807031@gmail.com>
2020-11-27 16:31:37 +0800

    Update dl_data setting path.

--- src/dl_data/cmake/lidarnet_weights.cmake
+++ src/dl_data/cmake/lidarnet_weights.cmake
@@ -1,102 +1,81 @@
 
     
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet/hino1/N90deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet/hino1/N90deg.pb"
     DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/hino1/N90deg.pb"
     MD5 420ba3df46a26938759f3496966cd064)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet/hino1/P0deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet/hino1/P0deg.pb"
     DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/hino1/P0deg.pb"
     MD5 a361e6f03227a0217e8b39f7afa3a522)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet/hino1/P90deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet/hino1/P90deg.pb"
     DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/hino1/P90deg.pb"
     MD5 38558b17a24d7c12cd04397d1309f50f)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet/hino1/P180deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet/hino1/P180deg.pb"
     DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/hino1/P180deg.pb"
     MD5 0998661ac647dccc2d9c0877cf79aaf4)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet/b1/N90deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet/b1/N90deg.pb"
     DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/b1/N90deg.pb"
     MD5 8864a050dd49b5f38f73ed86bf8fdb70)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet/b1/P0deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet/b1/P0deg.pb"
     DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/b1/P0deg.pb"
     MD5 b98d886e3e092ef712dbeba56ede6ed3)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet/b1/P90deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet/b1/P90deg.pb"
     DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/b1/P90deg.pb"
     MD5 20987797cb13281da9774bdfeacb8166)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet/b1/P180deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet/b1/P180deg.pb"
     DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/b1/P180deg.pb"
     MD5 264d4c18620853a1c4377e4a2cf3e8ac)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet_v2/b1/N90deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet_v2/b1/N90deg.pb"
     DESTINATION "${LIDARNET_V2_WEIGHTS_DIR}/SqueezeSegNet/b1/N90deg.pb"
     MD5 6cdc45f525f3a8ce6e327a39bd7dadb2)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet_v2/b1/P0deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet_v2/b1/P0deg.pb"
     DESTINATION "${LIDARNET_V2_WEIGHTS_DIR}/SqueezeSegNet/b1/P0deg.pb"
     MD5 fbafe57dce27c8c9f9e23aa9ac02036c)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet_v2/b1/P90deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet_v2/b1/P90deg.pb"
     DESTINATION "${LIDARNET_V2_WEIGHTS_DIR}/SqueezeSegNet/b1/P90deg.pb"
     MD5 851b09d29479e4df57560573a824db54)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet_v2/b1/P180deg.pb"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/SqueezeSegNet_v2/b1/P180deg.pb"
     DESTINATION "${LIDARNET_V2_WEIGHTS_DIR}/SqueezeSegNet/b1/P180deg.pb"
     MD5 b796ab75ebc7c022ff74d92b995d3e6c)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/PointPillars/C1/Car/pfe.onnx"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/PointPillars/Car/pfe.onnx"
     DESTINATION "${LIDARNET_POINTPILLARS_WEIGHTS_DIR}/car/pfe.onnx"
     MD5 86e283c08f3b7f4b41f95f167949ac00)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/PointPillars/C1/Car/rpn.onnx"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/PointPillars/Car/rpn.onnx"
     DESTINATION "${LIDARNET_POINTPILLARS_WEIGHTS_DIR}/car/rpn.onnx"
     MD5 97d3cd297adaa0c7929fbba093eaacb4)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/PointPillars/C1/Ped_Cyc/pfe.onnx"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/PointPillars/Ped_Cyc/pfe.onnx"
     DESTINATION "${LIDARNET_POINTPILLARS_WEIGHTS_DIR}/ped_cyc/pfe.onnx"
     MD5 89d6b99414581c17fa545b6897f0c0c0)
 
 download_file(
-    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/PointPillars/C1/Ped_Cyc/rpn.onnx"
+    URL "http://nas.itriadv.co:8888/git_data/B1/LiDAR_Models/PointPillars/Ped_Cyc/rpn.onnx"
     DESTINATION "${LIDARNET_POINTPILLARS_WEIGHTS_DIR}/ped_cyc/rpn.onnx"
     MD5 c0f4c1ec518e6c84ddd2b966c544f9f8)
-
-#download_file(
-#    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet_v2/b1/N90deg.pb"
-#    DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/b1/N90deg.pb"
-#    MD5 31d1e2ff56aa0d7f67f06d033e423794)
-
-#download_file(
-#    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet_v2/b1/P0deg.pb"
-#    DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/b1/P0deg.pb"
-#    MD5 391b26bf8e434381e785d4b031f1190e)
-
-#download_file(
-#    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet_v2/b1/P90deg.pb"
-#    DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/b1/P90deg.pb"
-#    MD5 a554c98d041a112dfa9fa14e425e2c6e)
-
-#download_file(
-#    URL "http://118.163.54.109:8888/Share/ADV/LiDAR_Models/SqueezeSegNet_v2/b1/P180deg.pb"
-#    DESTINATION "${LIDARNET_WEIGHTS_DIR}/SqueezeSegNet/b1/P180deg.pb"
-#    MD5 1abcf546c2456c43e67c9070b55f14f0)
-
