0b629684c42e196db6e17a03a3a20512b059783e
chtseng <chtseng@itri.org.tw>
2020-12-24 12:00:27 +0800

    Remove boot_scripts.

--- src/scripts/bus_env_cbus/boot_scripts/lidar/lidar_ip_forwarding.sh
+++
@@ -1,9 +0,0 @@
-iptables -A FORWARD -o enp8s0 -i enp1s0f1 -s 192.168.3.10/24 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -F POSTROUTING
-iptables -t nat -A POSTROUTING -o enp8s0 -j MASQUERADE
-
-iptables -A FORWARD -o enp8s0 -i enp0s31f6 -s 192.168.4.10/24 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -F POSTROUTING
-iptables -t nat -A POSTROUTING -o enp8s0 -j MASQUERADE
