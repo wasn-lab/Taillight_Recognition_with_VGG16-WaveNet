40f77779acd5e7f5a83d7c46ace363a84922aeaa
chtseng <chtseng@itri.org.tw>
2020-12-24 12:07:54 +0800

    Add sudo back.

--- src/scripts/bus_env_cbus/lidar/ip_forwarding.sh
+++ src/scripts/bus_env_cbus/lidar/ip_forwarding.sh
@@ -1,9 +1,9 @@
-iptables -A FORWARD -o enp8s0 -i enp1s0f1 -s 192.168.3.10/24 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -F POSTROUTING
-iptables -t nat -A POSTROUTING -o enp8s0 -j MASQUERADE
+sudo iptables -A FORWARD -o enp8s0 -i enp1s0f1 -s 192.168.3.10/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t nat -F POSTROUTING
+sudo iptables -t nat -A POSTROUTING -o enp8s0 -j MASQUERADE
 
-iptables -A FORWARD -o enp8s0 -i enp0s31f6 -s 192.168.4.10/24 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -F POSTROUTING
-iptables -t nat -A POSTROUTING -o enp8s0 -j MASQUERADE
+sudo iptables -A FORWARD -o enp8s0 -i enp0s31f6 -s 192.168.4.10/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t nat -F POSTROUTING
+sudo iptables -t nat -A POSTROUTING -o enp8s0 -j MASQUERADE
