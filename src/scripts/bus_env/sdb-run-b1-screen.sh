02be7f34eb322d8eb09e703501ef52c871a272e0
chtseng <chtseng@itri.org.tw>
2020-12-29 15:52:49 +0800

    Wait 5 seconds to set up ROS params.

--- src/scripts/bus_env/sdb-run-b1-screen.sh
+++ src/scripts/bus_env/sdb-run-b1-screen.sh
@@ -5,6 +5,7 @@ echo starting programs........
 readonly cur_dir=$(dirname $(readlink -e $0))
 
 gnome-terminal -e "screen -c ${cur_dir}/lidar.screen"
+sleep 5;
 gnome-terminal -e 'ssh -t ta "screen -c /home/nvidia/itriadv/src/scripts/bus_env/xavier.screen"'
 gnome-terminal -e 'ssh -t local "screen -c /home/local/itriadv/src/scripts/bus_env/localization.screen"'
 gnome-terminal -e "ssh -t camera 'screen -c /home/camera/itriadv/src/scripts/bus_env/camera.screen'"
