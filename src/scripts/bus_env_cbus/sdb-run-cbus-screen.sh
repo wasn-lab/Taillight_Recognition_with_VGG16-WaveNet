816abfcacd3977fca4a60a2271b6e96857edaac6
chtseng <chtseng@itri.org.tw>
2020-12-29 15:54:52 +0800

    Add messages.

--- src/scripts/bus_env_cbus/sdb-run-cbus-screen.sh
+++ src/scripts/bus_env_cbus/sdb-run-cbus-screen.sh
@@ -5,6 +5,7 @@ echo starting programs........
 readonly cur_dir=$(dirname $(readlink -e $0))
 
 gnome-terminal -e "screen -c ${cur_dir}/lidar.screen"
+echo "Wait a few seconds before bringing up other nodes."
 sleep 5
 gnome-terminal -e 'ssh -t xavier "screen -c /home/nvidia/itriadv/src/scripts/bus_env_cbus/xavier.screen"'
 gnome-terminal -e 'ssh -t local "screen -c /home/local/itriadv/src/scripts/bus_env_cbus/localization.screen"'
