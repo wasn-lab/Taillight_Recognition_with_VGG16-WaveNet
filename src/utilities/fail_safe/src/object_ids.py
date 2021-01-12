36ce78aadff6c887bdf36ae2aad6b5508df48726
chtseng <chtseng@itri.org.tw>
2021-01-12 15:49:40 +0800

    Add camera_detection_parser.py

---
+++ src/utilities/fail_safe/src/object_ids.py
@@ -0,0 +1,14 @@
+"""
+Object class id
+see src/msgs/include/detected_object_class_id.h
+"""
+
+OBJECT_ID_TO_NAME = {0: "Unknown",
+                     1: "Person",
+                     2: "Bicycle",
+                     3: "Motobike",
+                     4: "Car",
+                     5: "Bus",
+                     6: "Truck",
+                     7: "Sign",
+                     8: "Light"}
