b1b73525629c7c19c3521fa5b86438da9ca1941b
chtseng <chtseng@itri.org.tw>
2021-01-19 14:55:47 +0800

    Add --wait to wait the detection of roscore.

--- src/scripts/bus_env_cbus/lidar/run_sys_check_gateway.sh
+++ src/scripts/bus_env_cbus/lidar/run_sys_check_gateway.sh
@@ -3,4 +3,4 @@ source /home/lidar/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.3
 #source /home/lidar/py3env/bin/activate
-roslaunch issue_reporter sys_ready_check.launch
+roslaunch --wait issue_reporter sys_ready_check.launch
