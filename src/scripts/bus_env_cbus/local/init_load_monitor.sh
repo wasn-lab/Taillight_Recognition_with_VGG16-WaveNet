dd86076bea04e03b613add5478a1cc22472ecff0
chtseng <chtseng@itri.org.tw>
2021-01-20 15:09:42 +0800

    Add --wait to roslaunch load_monitor.

--- src/scripts/bus_env_cbus/local/init_load_monitor.sh
+++ src/scripts/bus_env_cbus/local/init_load_monitor.sh
@@ -4,4 +4,4 @@ source /home/local/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.5
 
-roslaunch fail_safe load_monitor.launch
+roslaunch --wait fail_safe load_monitor.launch
