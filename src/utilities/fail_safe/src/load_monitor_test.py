8c914694990589307a18b5a2b273769e44b9c81b
chtseng <chtseng@itri.org.tw>
2021-01-20 14:07:22 +0800

    Fall back to pure nvidia-smi output to get gpu load.

--- src/utilities/fail_safe/src/load_monitor_test.py
+++ src/utilities/fail_safe/src/load_monitor_test.py
@@ -1,5 +1,5 @@
 import unittest
-from load_monitor import get_hostname, LoadMonitor
+from load_monitor import get_hostname, LoadMonitor, _parse_nvidia_smi_output
 
 class LoadMonitorTest(unittest.TestCase):
     def setUp(self):
@@ -8,6 +8,13 @@ class LoadMonitorTest(unittest.TestCase):
     def test_get_gpu_load(self):
         gpu_load = self.monitor.get_gpu_load()
         self.assertTrue(len(gpu_load) > 0)
+
+    def test_get_gpu_load_fallback(self):
+        line = "| 22%   34C    P8     9W / 250W |     44MiB / 11016MiB |      17%      Default |"
+        self.assertEqual(_parse_nvidia_smi_output(line), "17")
+
+        line = "|   0  GeForce RTX 208...  Off  | 00000000:06:00.0 Off |                  N/A |"
+        self.assertEqual(_parse_nvidia_smi_output(line), "")
 
     def test_get_cpu_load(self):
         cpu_load = self.monitor.get_cpu_load()
