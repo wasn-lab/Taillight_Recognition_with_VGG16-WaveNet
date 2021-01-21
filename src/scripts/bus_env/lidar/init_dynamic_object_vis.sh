b48b7ec99cf3c0a8628747e0872b6aed1bf44cb3
Ching-Hao Liu <chinghao.liu@itri.org.tw>
2021-01-21 16:01:58 +0800

    Add --wait for roslaunch.

--- src/scripts/bus_env/lidar/init_dynamic_object_vis.sh
+++ src/scripts/bus_env/lidar/init_dynamic_object_vis.sh
@@ -5,4 +5,4 @@ source /home/lidar/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.3
 
-roslaunch dynamic_object_visualization dynamic_object_visualizer.launch input:=objects
+roslaunch --wait dynamic_object_visualization dynamic_object_visualizer.launch input:=objects
