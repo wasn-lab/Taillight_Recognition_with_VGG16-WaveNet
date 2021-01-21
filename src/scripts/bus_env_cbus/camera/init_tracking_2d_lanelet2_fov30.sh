6c5ca4a3a6785c6b6c2b99a2cc34224ce20a36dd
Ching-Hao Liu <chinghao.liu@itri.org.tw>
2021-01-21 15:23:45 +0800

    Move init_tracking_2d from lidar_ipc to camera_ipc.

--- src/scripts/bus_env_cbus/camera/init_tracking_2d_lanelet2_fov30.sh
+++ src/scripts/bus_env_cbus/camera/init_tracking_2d_lanelet2_fov30.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-source /home/lidar/itriadv/devel/setup.bash
+source /home/camera/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
-export ROS_IP=192.168.1.3
+export ROS_IP=192.168.2.1
 
 roslaunch --wait itri_tracking_2d track2d.launch input_source:=1
