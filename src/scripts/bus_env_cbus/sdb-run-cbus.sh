d0a156cb70533024e932e126d5f55262db22104a
Ching-Hao Liu <chinghao.liu@itri.org.tw>
2021-01-21 16:22:31 +0800

    Modify command for tracking_2d.

--- src/scripts/bus_env_cbus/sdb-run-cbus.sh
+++ src/scripts/bus_env_cbus/sdb-run-cbus.sh
@@ -81,13 +81,13 @@ sleep 3
 gnome-terminal  -e 'ssh -t xavier "/home/nvidia/itriadv/src/scripts/bus_env_cbus/xavier/init_camera.sh;exec bash"'
 
 sleep 3
-gnome-terminal  -e 'bash -c "/home/lidar/itriadv/src/scripts/bus_env_cbus/lidar/init_tracking_2d.sh;exec bash"'
+gnome-terminal  -e "ssh -t camera '/home/camera/itriadv/src/scripts/bus_env_cbus/camera/init_tracking_2d.sh;exec bash'"
 sleep 3
-gnome-terminal  -e 'bash -c "/home/lidar/itriadv/src/scripts/bus_env_cbus/lidar/init_tracking_2d_left.sh;exec bash"'
+gnome-terminal  -e "ssh -t camera '/home/camera/itriadv/src/scripts/bus_env_cbus/camera/init_tracking_2d_left.sh;exec bash'"
 sleep 3
-gnome-terminal  -e 'bash -c "/home/lidar/itriadv/src/scripts/bus_env_cbus/lidar/init_tracking_2d_right.sh;exec bash"'
+gnome-terminal  -e "ssh -t camera '/home/camera/itriadv/src/scripts/bus_env_cbus/camera/init_tracking_2d_right.sh;exec bash'"
 sleep 3
-gnome-terminal  -e 'bash -c "/home/lidar/itriadv/src/scripts/bus_env_cbus/lidar/init_tracking_2d_fov30.sh;exec bash"'
+gnome-terminal  -e "ssh -t camera '/home/camera/itriadv/src/scripts/bus_env_cbus/camera/init_tracking_2d_fov30.sh;exec bash'"
 sleep 3
 gnome-terminal  -e 'bash -c "/home/lidar/itriadv/src/scripts/bus_env_cbus/lidar/init_pedcross.sh;exec bash"'
 sleep 3
