3af1e8a92d26b13e5516ddd8a63ddc4ca1e0cec1
chtseng <chtseng@itri.org.tw>
2021-01-22 16:08:36 +0800

    Add publish test.

--- src/utilities/pc2_compressor/src/test/run_publish_test.sh
+++ src/utilities/pc2_compressor/src/test/run_publish_test.sh
@@ -5,7 +5,7 @@ readonly repo_dir=$(git rev-parse --show-toplevel)
 pushd $repo_dir/build
 source $repo_dir/devel/setup.bash
 
-make -j lidar_raw_2020-03-10-10-48-39_41.bag
+make lidar_raw_2020-03-10-10-48-39_41_1s.bag
 rostest pc2_compressor publish_test_cmpr.test
 
 popd
