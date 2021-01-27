82a51fd5079b52a95b66ba5970f08c0d4b053bd7
chtseng <chtseng@itri.org.tw>
2021-01-27 11:15:20 +0800

    Move load_monitor to boot up time.

--- src/scripts/bus_env_cbus/local/init_load_monitor.sh
+++ src/scripts/bus_env_cbus/local/init_load_monitor.sh
@@ -1,7 +1,13 @@
-#! /bin/bash
-source /home/local/itriadv/devel/setup.bash
-
+#!/bin/bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.5
 
+until ls /home/local/itriadv/devel/setup.bash
+do
+  echo "Wait auto mount operational"
+  sleep 1
+done
+
+export HOSTNAME=`hostname`
+source /home/local/itriadv/devel/setup.bash
 roslaunch --wait fail_safe load_monitor.launch
