2eb6c3ab1f8c4bdc4be0891d653ae2299f28885a
chtseng <chtseng@itri.org.tw>
2021-01-28 10:26:51 +0800

    Move fail_safe to camera IPC as lidar IPC cannot receive xavier heartbeats.

--- src/scripts/bus_env_cbus/camera/init_fail_safe.sh
+++ src/scripts/bus_env_cbus/camera/init_fail_safe.sh
@@ -3,6 +3,6 @@
 source /home/lidar/itriadv/devel/setup.bash
 #source /home/lidar/workspace/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
-export ROS_IP=192.168.1.3
+export ROS_IP=192.168.2.1
 
 roslaunch --wait fail_safe fail_safe.launch
