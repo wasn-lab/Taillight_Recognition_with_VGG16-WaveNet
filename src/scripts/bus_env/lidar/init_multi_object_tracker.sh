450e080e2e4ea88c012bb8679bf1c95435c1b711
icl.u300 <icl.u300@itri.org.tw>
2021-01-31 14:13:28 +0800

    Fix typos.

--- src/scripts/bus_env/lidar/init_multi_object_tracker.sh
+++ src/scripts/bus_env/lidar/init_multi_object_tracker.sh
@@ -4,4 +4,4 @@ source /home/lidar/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.3
 
-roslaunch --wait roslaunch multi_object_tracker multi_object_tracker.launch
+roslaunch --wait multi_object_tracker multi_object_tracker.launch
