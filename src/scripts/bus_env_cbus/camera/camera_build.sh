3f902639f340edc78436f01c50f408e47ce72a48
lidar_ipc_lab <liadr_ipc_lab@itri.org.tw>
2021-02-23 10:11:04 +0800

    Do not use readlink outside lidar ipc.

--- src/scripts/bus_env_cbus/camera/camera_build.sh
+++ src/scripts/bus_env_cbus/camera/camera_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 
-script_dir=$(dirname $(readlink -e $0))
-repo_dir=$(readlink -e ${script_dir}/../../../..)
+script_dir=$(dirname $0)
+repo_dir=${script_dir}/../../../..
 
 pushd $repo_dir
 catkin_make -DCMAKE_BUILD_TYPE=Release -DCAR_MODEL=C1
