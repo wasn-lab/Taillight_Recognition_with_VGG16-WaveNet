794e7ead27c227af11fd6d3e151fba9cce260b15
chtseng <chtseng@itri.org.tw>
2021-02-24 14:11:07 +0800

    preload opencv4 when running test.

--- src/scripts/ci/run_unittest.sh
+++ src/scripts/ci/run_unittest.sh
@@ -15,6 +15,7 @@ pushd $repo_dir/build
 
 # Let catkin_make builds only the target run_tests.
 make -j car_model_test camera_utils_test lidar_test
+export LD_PRELOAD=/usr/local/lib/libopencv_core.so
 ../devel/lib/car_model/car_model_test
 ../devel/lib/camera_utils/camera_utils_test
 ../devel/lib/libs/lidar_test
