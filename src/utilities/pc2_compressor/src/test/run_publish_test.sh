55cd4f567bd33873716534e52a6e04f0efa592d4
chtseng <chtseng@itri.org.tw>
2021-02-25 10:46:52 +0800

    Do not show verbose message when running source.

--- src/utilities/pc2_compressor/src/test/run_publish_test.sh
+++ src/utilities/pc2_compressor/src/test/run_publish_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -x
 set -e
 readonly repo_dir=$(git rev-parse --show-toplevel)
 pushd $repo_dir/build
 source $repo_dir/devel/setup.bash
 
+set -x
 make lidar_raw_2020-03-10-10-48-39_41_1s.bag
 rostest pc2_compressor publish_test_cmpr.test
 
