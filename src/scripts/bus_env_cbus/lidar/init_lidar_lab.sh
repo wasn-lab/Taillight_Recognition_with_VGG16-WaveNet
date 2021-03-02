36b71647a7a48f3dccc6228cfeb7bce2ce7e97fa
lidar_ipc_lab <liadr_ipc_lab@itri.org.tw>
2021-03-02 11:15:01 +0800

    Refactor C1 startup scripts in the lab env.

---
+++ src/scripts/bus_env_cbus/lidar/init_lidar_lab.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+source /home/lidar/itriadv/devel/setup.bash
+export ROS_MASTER_URI=http://192.168.1.3:11311
+export ROS_IP=192.168.1.3
+roslaunch --wait lidar lab.launch
+#roslaunch lidar b1.launch hardware_enable:=0
+#roslaunch sdb lidar.launch
+
