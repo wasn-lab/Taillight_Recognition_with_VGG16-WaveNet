7b0eccea72f591a95a9bac65a35fb5a1936c048a
uubuntu <boyuchen@itir.org.tw>
2021-03-05 17:15:21 +0800

    modified:   src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/src/LightNet.cpp 	modified:   src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/src/LightNet.h 	modified:   src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/src/main.cpp 	modified:   src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/src/main.h

--- src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/src/LightNet.h
+++ src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/src/LightNet.h
@@ -60,14 +60,14 @@ const std::string INPUT_CAFFEMODEL_30deg = "./src/sensing/itri_lightnet_new_layo
 string mode_30deg = "fp16";
 RUN_MODE run_mode_30deg = RUN_MODE::FLOAT32;
 string OUTPUTS = "yolo-det"; //layer82-conv,layer94-conv,layer106-conv
-string engineName_30deg = "./src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/resources/yolov3_fp16_201208_30deg.engine";
+string engineName_30deg = "/resources/yolov3_fp16_201208_30deg.engine";
 unique_ptr<float[]> outputData_30deg(new float[402193]);
 
 const std::string INPUT_PROTOTXT_60deg = "./src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/caffe_model/60deg/60_divU.prototxt";
 const std::string INPUT_CAFFEMODEL_60deg = "./src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/caffe_model/60deg/60_divU.caffemodel";
 string mode_60deg = "fp16";
 RUN_MODE run_mode_60deg = RUN_MODE::FLOAT16;
-string engineName_60deg = "./src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/resources/yolov3_fp16_201208_60deg.engine";
+string engineName_60deg = "/resources/yolov3_fp16_201208_60deg.engine";
 unique_ptr<float[]> outputData_60deg(new float[402193]);
 #endif
 
