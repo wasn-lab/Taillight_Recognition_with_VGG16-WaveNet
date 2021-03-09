3389b22100e628999b3de2dc881d286c900b21bd
uubuntu <boyuchen@itir.org.tw>
2021-03-09 08:34:26 +0800

    modified:   src/dl_data/cmake/lightnet_weights.cmake 	modified:   src/sensing/itri_lightnet_new_layout/itri_lightnet_new_layout/CMakeLists.txt

--- src/dl_data/cmake/lightnet_weights.cmake
+++ src/dl_data/cmake/lightnet_weights.cmake
@@ -9,3 +9,9 @@ download_file(
     DESTINATION "${LIGHTNET_WEIGHTS_DIR}/iclu60_v3-kINT8-batch1.engine"
     MD5 074adae687675d17c6446718beaf074f
 )
+
+download_file(
+   URL "140.96.172.66:8080/share.cgi?ssid=0RTQaYe&fid=0RTQaYe&filename=libdetector.so&openfolder=forcedownload&ep="
+    DESTINATION "${LIGHTNET_WEIGHTS_DIR}/libdetector.so"
+    MD5 c3543102328474ae57ed3d18f38e69bd
+)
