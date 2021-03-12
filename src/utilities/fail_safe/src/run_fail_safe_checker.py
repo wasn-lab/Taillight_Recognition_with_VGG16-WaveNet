8d2965eadc9c4952f37a760b1b72096cf0acb2f0
chtseng <chtseng@itri.org.tw>
2021-03-12 08:51:30 +0800

    Use more generic ini filename mqtt_b1.ini.

--- src/utilities/fail_safe/src/run_fail_safe_checker.py
+++ src/utilities/fail_safe/src/run_fail_safe_checker.py
@@ -13,7 +13,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--heartbeat-ini", default=os.path.join(src_dir, "heartbeat.ini"))
-    parser.add_argument("--mqtt-ini", default=os.path.join(src_dir, "mqtt_b1_v2.ini"))
+    parser.add_argument("--mqtt-ini", default=os.path.join(src_dir, "mqtt_b1.ini"))
     parser.add_argument("--mqtt-fqdn", default=None, help="Use the mqtt fqdn given in command line.")
     parser.add_argument("--debug-mode", action="store_true")
     args = parser.parse_known_args()[0]
