eb1d269035b49ac3e59d42b5e7f565e3a544f2c6
chtseng <chtseng@itri.org.tw>
2021-03-30 13:01:12 +0800

    Skip init_test_env script if possible.

--- src/car_model/test_car_b1_v3/publish_test.sh
+++ src/car_model/test_car_b1_v3/publish_test.sh
@@ -13,14 +13,14 @@ if [[ ! -f ${bag_dir}/camera_raw_2021-02-25-15-53-00_77.bag
   || ! -f ${bag_dir}/auto_record_2020-11-04-15-29-32_0.bag
   || ! -f ${bag_dir}/auto_record_2020-04-14-16-41-15_89.bag
   || ! -f ${bag_dir}/geofence_pp_2020-11-16-16-35-39.bag
-  || ! -f ${bag_dir}/lidar_raw_2020-03-10-10-48-39_41.bag
-  || ! -f ${bag_dir}/lidar_raw_compressed_2021-01-12-16-01-42.bag
+  || ! -f ${bag_dir}/lidar_raw_compressed_2021-02-03.bag
   || ! -f ${bag_dir}/lidar_detection_car_ped_cyc_2020-12-28-16-53-14_21.bag
+  || ! -f ${bag_dir}/lidar_raw_2020-12-28-16-53-14_21.bag
   || ! -f ${bag_dir}/localization_raw_2020-09-24-17-02-06.bag
+  || ! -f ${bag_dir}/lidarxyz2lla_2020-04-13-17-45-48_0.bag
   || ! -f ${bag_dir}/rad_grab_2020-04-13-17-45-48_0.bag
   || ! -f ${bag_dir}/ukf_mm_2020-04-13-17-45-48_0.bag
   || ! -f ${bag_dir}/target_planner_2020-04-13-17-45-48_0.bag
-  || ! -f ${bag_dir}/lidarxyz2lla_2020-04-13-17-45-48_0.bag
   || ! -f ${bag_dir}/auto_record_2020-06-19-16-26-18_1_filtered.bag
   || ! -f ${bag_dir}/tracking_2d_2020-11-16-15-02-12.bag ]]; then
   bash src/car_model/test_car_b1_v3/init_test_env.sh
