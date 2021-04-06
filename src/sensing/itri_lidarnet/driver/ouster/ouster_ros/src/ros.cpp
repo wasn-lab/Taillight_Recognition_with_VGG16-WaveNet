a86ac98728e14c83a7184d8772a151a7d06723e5
MikeChan <m7807031@gmail.com>
2021-04-06 18:47:48 +0800

    Update for new ouster driver time stamp issue.

--- src/sensing/itri_lidarnet/driver/ouster/ouster_ros/src/ros.cpp
+++ src/sensing/itri_lidarnet/driver/ouster/ouster_ros/src/ros.cpp
@@ -94,6 +94,7 @@ sensor_msgs::PointCloud2 cloud_to_cloud_msg(const Cloud& cloud, ns timestamp,
     pcl::toROSMsg(cloud, msg);
     msg.header.frame_id = frame;
     msg.header.stamp.fromNSec(timestamp.count());
+    msg.header.stamp = ros::Time::now();
     return msg;
 }
 
