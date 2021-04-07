5fa463ce0aba256d81bb306969e26111030a7db6
MikeChan <m7807031@gmail.com>
2021-04-07 15:41:16 +0800

    Update startup scripts for car B1.

--- src/scripts/bus_env/camera/init_camera.sh
+++ src/scripts/bus_env/camera/init_camera.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 source /home/camera/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
-export ROS_IP=192.168.2.1
+export ROS_IP=192.168.2.4
 roslaunch --wait sdb camera_b1_v3.launch
 
--- src/scripts/bus_env/camera/init_tracking_2d.sh
+++ src/scripts/bus_env/camera/init_tracking_2d.sh
@@ -2,6 +2,6 @@
 
 source /home/camera/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
-export ROS_IP=192.168.2.1
+export ROS_IP=192.168.2.4
 
 roslaunch --wait itri_tracking_2d track2d.launch input_source:=0
