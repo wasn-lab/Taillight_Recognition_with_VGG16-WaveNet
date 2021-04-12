0ba121f32e95a2f66ab83ef174596d37d9e1d00a
lidar_c1 <lidar_c1@example.com>
2021-04-12 17:52:36 +0800

    Update C1 startup scripts for camera IPC.

--- src/scripts/bus_env_cbus/camera/init_fail_safe.sh
+++ src/scripts/bus_env_cbus/camera/init_fail_safe.sh
@@ -2,6 +2,6 @@
 
 source /home/camera/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
-export ROS_IP=192.168.2.1
+export ROS_IP=192.168.1.4
 
 roslaunch --wait fail_safe fail_safe.launch
