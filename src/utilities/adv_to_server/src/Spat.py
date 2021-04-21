0cadf25e9fc2840da79307469b6c9c4fba106e57
MikeChan <m7807031@gmail.com>
2021-04-21 14:12:56 +0800

    modify spat.py

--- src/utilities/adv_to_server/src/Spat.py
+++ src/utilities/adv_to_server/src/Spat.py
@@ -2,11 +2,11 @@
 import socket
 import json
 import time
-addr=('192.168.43.204',8766)
+addr=('192.168.1.3',8766)
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 count = 0
 while 1:
-    data = {"SPaT_MAP_Info":{"Latitude": 123.456,  "Longitude":456.789, "Spat_state" : 101, "Spat_sec": 15.5, "Signal_state":0, "Index": count} }
+    data = {"SPaT_MAP_Info":{"intersection_id": 0, "road_id": 001, "Latitude": 123.456,  "Longitude":456.789, "Spat_state" : 101, "Spat_sec": 15.5, "Signal_state":0, "Index": count} }
     if not data:
         break
     s.sendto(json.dumps(data),addr)
