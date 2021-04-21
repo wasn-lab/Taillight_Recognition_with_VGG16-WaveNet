0a0fac1d89c8ce1a1e10e02fcc572f6a0383dc04
MikeChan <m7807031@gmail.com>
2021-04-21 09:05:46 +0800

    Update for Ouster boot up normally.

--- src/scripts/bus_env_cbus/sdb-run-cbus-screen.sh
+++ src/scripts/bus_env_cbus/sdb-run-cbus-screen.sh
@@ -9,6 +9,9 @@ rosparam set /south_bridge/company_name itri
 rosparam set /car_model C1
 
 gnome-terminal -e "screen -c ${cur_dir}/lidar.screen"
-gnome-terminal -e 'ssh -t xavier "screen -c /home/nvidia/itriadv/src/scripts/bus_env_cbus/xavier.screen"'
+
 gnome-terminal -e 'ssh -t local "screen -c /home/local/itriadv/src/scripts/bus_env_cbus/localization.screen"'
 gnome-terminal -e "ssh -t camera 'screen -c /home/camera/itriadv/src/scripts/bus_env_cbus/camera.screen'"
+
+sleep 30
+gnome-terminal -e 'ssh -t xavier "screen -c /home/nvidia/itriadv/src/scripts/bus_env_cbus/xavier.screen"'
