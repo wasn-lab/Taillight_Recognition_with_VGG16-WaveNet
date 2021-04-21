7116ca7fa9478ffcafc5685255f3f445f1ffdb79
chtseng <chtseng@itri.org.tw>
2021-04-21 14:05:54 +0800

    Move Chromium window to the monitor in the driver cabinet.

--- src/scripts/bus_env/lidar/run_hmi.sh
+++ src/scripts/bus_env/lidar/run_hmi.sh
@@ -4,3 +4,5 @@ sleep 30;
 # Old setting, streamed images freeze after 8 seconds.
 # firefox "https://service.itriadv.co:8784/Unit/DriverDashboard?URL=local&R=true"
 chromium-browser "http://service.itriadv.co:8785/Unit/DriverDashboard?URL=local&R=true"
+sleep 10
+python /usr/local/bin/move_window.py -m DP-0 -w Chromium
---
+++ src/scripts/deploy/move_window.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+"""
+python move_window.py --window-title <title> --monitor <monitor>
+
+Use xrandr to find out monitor. Usually monitor is HDMI-0, DP-0 etc.
+"""
+import argparse
+import sys
+import subprocess
+import re
+
+def _installed(prog):
+    found = False
+    try:
+        subprocess.check_call(["which", prog])
+        print("Successfully find " + prog + ". Keep going.")
+        found = True
+    except subprocess.CalledProcessError:
+        print("Oh! You need to install " + prog)
+    return found
+
+
+def find_window_id(window_title):
+    output = subprocess.check_output(["wmctrl", "-l"]).decode("utf-8")
+    for line in output.splitlines():
+        if window_title not in line:
+            continue
+        wid = line.split()[0]
+        print("Find window id: {}".format(wid))
+        return wid
+    print("Cannot find window id for {}".format(window_title))
+    return ""
+
+
+def maximize_window(wid):
+    cmd = ["xdotool", "windowsize", "-sync", wid, "100%", "100%"]
+    print(" ".join(cmd))
+    subprocess.check_call(cmd)
+
+def move_window(wid, monitor):
+    xrandr_out = subprocess.check_output(["xrandr"]).decode("utf-8")
+    hpos = ""
+    for line in xrandr_out.splitlines():
+        if ("connected" not in line) or (monitor not in line):
+            continue
+        fields = line.split()
+        for field in fields:
+            if field.count("+") != 2:
+                continue
+            hpos = field.split("+")[-2]
+            print("Find horizontal offset {} for monitor {}".format(hpos, monitor))
+    if not hpos:
+        print("Cannot find horizontal offset for monitor " + monitor)
+        return
+
+    cmd = ["wmctrl", "-ir", wid, "-e", "0,"+str(int(hpos)+100)+",100,300,300"]
+    print(" ".join(cmd))
+    subprocess.check_call(cmd)
+
+def move_and_max(window_title, monitor):
+    wid = find_window_id(window_title)
+    if not wid:
+        return
+    move_window(wid, monitor)
+    maximize_window(wid)
+
+def main():
+    for prog in ["xrandr", "wmctrl", "xdotool"]:
+        if not _installed(prog):
+            sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--window-title", "-w", required=True)
+    parser.add_argument("--monitor", "-m", required=True)
+    args = parser.parse_args()
+    move_and_max(args.window_title, args.monitor)
+
+if __name__ == "__main__":
+    main()
