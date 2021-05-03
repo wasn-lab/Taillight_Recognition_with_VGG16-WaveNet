a430bedfb1231369f202313f2f6025e13d103fb0
chtseng <chtseng@itri.org.tw>
2021-05-03 13:12:16 +0800

    Add fps and driving mode options.

--- src/utilities/fail_safe/src/mock_ctrl_info02.py
+++ src/utilities/fail_safe/src/mock_ctrl_info02.py
@@ -2,19 +2,20 @@
 # Copyright (c) 2021, Industrial Technology and Research Institute.
 # All rights reserved.
 from __future__ import print_function
+import argparse
 import sys
 import rospy
 from msgs.msg import Flag_Info
 
 
-def main():
-    node_name = "FakeFlagInfo02"
+def gen_flag02(fps, self_driving_mode):
+    node_name = "MockCtrlInfo02Generator"
     rospy.init_node(node_name)
     rospy.logwarn("Init %s", node_name)
-    rate = rospy.Rate(10)  # FPS: 10
+    rate = rospy.Rate(fps)
 
     msg = Flag_Info()
-    msg.Dspace_Flag08 = 1.0
+    msg.Dspace_Flag08 = float(self_driving_mode)
 
     publisher = rospy.Publisher("/Flag_Info02", Flag_Info, queue_size=1)
     print("Keep publishing msg.Dspace_Flag08 = {}".format(msg.Dspace_Flag08))
@@ -22,6 +23,14 @@ def main():
         publisher.publish(msg)
         rate.sleep()
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fps", type=int, default=10)
+    parser.add_argument("--self-driving-mode", type=int, default=1,
+                        help="1: self-driving, 0: manual-driving")
+    args = parser.parse_known_args()[0]
+    gen_flag02(args.fps, args.self_driving_mode)
 
 if __name__ == "__main__":
     try:
