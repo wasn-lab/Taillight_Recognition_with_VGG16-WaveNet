02e66aa2539d87209d1802d607647aff127b227a
amtommy6 <amtommy6@gmail.com>
2021-05-11 14:00:30 +0800

    Modify scripts for C1

--- src/scripts/bus_env_cbus/sdb-run-cbus-lab-screen.sh
+++ src/scripts/bus_env_cbus/sdb-run-cbus-lab-screen.sh
@@ -11,7 +11,8 @@ rosparam set /car_model C1
 gnome-terminal -e "screen -c ${cur_dir}/lidar-lab.screen"
 
 gnome-terminal -e 'ssh -t local "screen -c /home/local/itriadv/src/scripts/bus_env_cbus/localization.screen"'
-gnome-terminal -e "ssh -t camera 'screen -c /home/camera/itriadv/src/scripts/bus_env_cbus/camera.screen'"
 
 sleep 30
 gnome-terminal -e 'ssh -t xavier "screen -c /home/nvidia/itriadv/src/scripts/bus_env_cbus/xavier.screen"'
+sleep 5
+gnome-terminal -e "ssh -t camera 'screen -c /home/camera/itriadv/src/scripts/bus_env_cbus/camera.screen'"
