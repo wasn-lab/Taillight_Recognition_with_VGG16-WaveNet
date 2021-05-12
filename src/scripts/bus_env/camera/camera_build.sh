6de71a0c3619d844435cd4c980b278d6afa2a3f6
amtommy6 <amtommy6@gmail.com>
2021-05-12 16:29:58 +0800

    Remove Build type

--- src/scripts/bus_env/camera/camera_build.sh
+++ src/scripts/bus_env/camera/camera_build.sh
@@ -6,5 +6,4 @@ repo_dir=${script_dir}/../../../..
 
 pushd $repo_dir
 catkin_make -DCATKIN_WHITELIST_PACKAGES="car_model;msgs;autoware_perception_msgs;camera_utils;camera_grabber;image_compressor;object_costmap_generator;drivenet_lib;drivenet;libs;alignment;fail_safe;hungarian;itri_tracking_2d;msg_replay;msg_recorder" -DCAR_MODEL=B1_V3
--DCMAKE_BUILD_TYPE=Release
 popd
