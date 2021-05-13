3247c898f9135038670af5e0a3d1a619dc97376c
chtseng <chtseng@itri.org.tw>
2021-05-13 10:30:08 +0800

    Clean up start-up scripts in lidar IPC.

--- src/scripts/bus_env/lidar/run_gui_gateway.sh
+++ src/scripts/bus_env/lidar/run_gui_gateway.sh
@@ -1,6 +1,5 @@
-#! /bin/bash
+#!/bin/bash
 source /home/lidar/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.3
-#source /home/lidar/py3env/bin/activate
 roslaunch --wait opengl_test GUI_gateway.launch
