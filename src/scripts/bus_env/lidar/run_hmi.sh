6c55571b26e60ce083274e68703c92401baad7b2
TH <t.h.liu@itri.org.tw>
2021-05-14 21:14:45 +0800

    Browser 換成 Opera

--- src/scripts/bus_env/lidar/run_hmi.sh
+++ src/scripts/bus_env/lidar/run_hmi.sh
@@ -2,7 +2,7 @@
 sleep 10;
 # Old setting, streamed images freeze after 8 seconds.
 # firefox "https://service.itriadv.co:8784/Unit/DriverDashboard?URL=local&R=true"
-chromium-browser --password-store=basic --kiosk --app="http://service.itriadv.co:8785/Unit/DriverDashboard?URL=local&R=true" &
+opera "http://service.itriadv.co:8785/Unit/DriverDashboard?URL=local&R=true" &
 sleep 5 
 python /usr/local/bin/move_window.py -m DP-5 -w "駕駛艙畫面"
 echo "run infinite loop to raise HMI to be the top window."
