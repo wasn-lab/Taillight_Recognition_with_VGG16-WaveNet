dc899d2570ed5bf9a3965157547c69e4092e5735
chtseng <chtseng@itri.org.tw>
2021-05-17 11:11:24 +0800

    Explicitly delay the launch for camera-related topics.

--- src/scripts/bus_env/lidar/init_detection_viz.sh
+++ src/scripts/bus_env/lidar/init_detection_viz.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
+set -x
 source /home/lidar/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.3
-sleep 50
+
+readonly PWD=`pwd`
+
+roscd car_model
+python scripts/check_ros_master.py --wait-until-alive
+python scripts/wait_topic.py --topic-name /LidarDetection
+python scripts/wait_topic.py --topic-name /CameraDetection
+python scripts/wait_topic.py --topic-name /current_pose
+
+cd $PWD
 roslaunch detection_viz itriadv_viz.launch rviz_config:="U3_b5_lanelet2_pedcross"
