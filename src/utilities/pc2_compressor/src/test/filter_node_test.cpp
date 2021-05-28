cc282481ba6ce66641bdf4dddae5665a0c1642dc
chtseng <chtseng@itri.org.tw>
2021-05-28 14:00:51 +0800

    WIP: check XYZIR values.

--- src/utilities/pc2_compressor/src/test/filter_node_test.cpp
+++ src/utilities/pc2_compressor/src/test/filter_node_test.cpp
@@ -11,6 +11,7 @@
 #include "pc2_compressor_test_utils.h"
 #include "pc2_compression_format.h"
 #include "filter_node.h"
+// #include "pointxyzir.h"
 
 TEST(PC2FilterTest, test_warmup)
 {
@@ -26,6 +27,11 @@ TEST(PC2FilterTest, test_filter_ouster64_pc2)
   EXPECT_EQ(msg->fields[2].name, "z");
   EXPECT_EQ(msg->fields[3].name, "intensity");
   EXPECT_EQ(msg->fields[4].name, "ring");
+  EXPECT_EQ(msg->header, g_org_ros_pc2_ptr->header);
+  EXPECT_EQ(msg->width, 1024);
+  EXPECT_EQ(msg->height, 64);
+  EXPECT_EQ(msg->is_bigendian, g_org_ros_pc2_ptr->is_bigendian);
+  EXPECT_EQ(msg->is_dense, g_org_ros_pc2_ptr->is_dense);
 
   int32_t org_size = pc2_compressor::size_of_msg(g_org_ros_pc2_ptr);
   int32_t result_size = pc2_compressor::size_of_msg(msg);
@@ -33,5 +39,18 @@ TEST(PC2FilterTest, test_filter_ouster64_pc2)
 
   LOG(INFO) << "Filtered size: " << result_size << ", org size: " << org_size << ", ratio: " << ratio;
 
+  // Check x, y, z, intensity
+  pcl::PCLPointCloud2 before_pc2;
+  pcl::PointCloud<pcl::PointXYZI>::Ptr before_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  pcl_conversions::toPCL(*g_org_ros_pc2_ptr, before_pc2);
+  pcl::fromPCLPointCloud2(before_pc2, *before_cloud);
+
+  // TODO: Segmentation fault (core dumped)
+/*
+  pcl::PCLPointCloud2 after_pc2;
+  pcl_conversions::toPCL(*msg, after_pc2);
+  pcl::PointCloud<PointXYZIR>::Ptr after_cloud(new pcl::PointCloud<PointXYZIR>);
+  pcl::fromPCLPointCloud2(after_pc2, *after_cloud);
+  */
 }
 
