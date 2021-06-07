828e5ea5997e01842aa7afe0d2a8a6c38fdff1c5
chtseng <chtseng@itri.org.tw>
2021-06-07 16:03:36 +0800

    Publish lidar zone images.

--- src/utilities/xwin_grabber/src/xwin_grabber_node.h
+++ src/utilities/xwin_grabber/src/xwin_grabber_node.h
@@ -9,12 +9,15 @@ class XWinGrabberNode
 {
 private:
   ros::Publisher jpg_publisher_;
+  ros::Publisher lidar_zone_jpg_publisher_;
   ros::Publisher raw_publisher_;
   ros::Publisher heartbeat_publisher_;
   ros::NodeHandle node_handle_;
   XWinGrabber grabber_;
 
   void streaming_xwin();
+  void publish_lidar_zone_img(const cv::Mat& img);
+  void publish_full_img(cv::Mat& img);
 
 public:
   XWinGrabberNode(const std::string&& xwin_title);
