6829a7a0c87568932c1e3d67ccfa05f747077691
chtseng <chtseng@itri.org.tw>
2021-06-22 16:55:38 +0800

    Add pc_transform test into mr pipeline.

--- src/sensing/itri_lidarnet/lidars_transform/src/test/run_unittest.sh
+++ src/sensing/itri_lidarnet/lidars_transform/src/test/run_unittest.sh
@@ -5,6 +5,6 @@ readonly repo_dir=$(git rev-parse --show-toplevel)
 pushd $repo_dir/build
 
 make -j pc_transform_test
-../devel/lib/pc2_compressor/pc_transform_test
+../devel/lib/pc_transform/pc_transform_test
 
 popd
