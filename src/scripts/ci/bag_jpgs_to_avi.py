2562147a8aff6047ce0a1df2bf131595a118d233
chtseng <chtseng@itri.org.tw>
2021-06-23 11:19:47 +0800

    Calculate fps dynamically.

--- src/scripts/ci/bag_jpgs_to_avi.py
+++ src/scripts/ci/bag_jpgs_to_avi.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import argparse
+import datetime
 import rosbag
 import numpy as np
 import cv2
@@ -7,20 +8,32 @@ import cv2
 
 def _save_avi(bag_filename, topic, avi_filename):
     bag = rosbag.Bag(bag_filename)
-    avi = None
-    #cv2.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
+#cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+    images = []
+    timestamps = []
     for _topic, msg, timestamp in bag.read_messages(topics=[topic]):
+        timestamps.append(timestamp)
         np_arr = np.fromstring(msg.data, np.uint8)
         image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-        if avi is None:
-            width = image.shape[1]
-            height = image.shape[0]
-            fps = 15.0 
-            fourcc = cv2.VideoWriter_fourcc(*'XVID')
-            avi = cv2.VideoWriter(avi_filename, fourcc, fps, (width, height))
-        avi.write(image)
+        images.append(image)
     bag.close()
-    if avi:
+
+    if images:
+        width = images[0].shape[1]
+        height = images[0].shape[0]
+
+        first_dt = datetime.datetime.fromtimestamp(timestamps[0])
+        last_dt = datetime.datetime.fromtimestamp(timestamps[-1])
+        delta = last_dt - first_dt
+        duration_in_second = delta.seconds
+        if duration_in_second <= 0:
+            duration_in_second = 1
+
+        fps = len(images) / float(duration_in_second)
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        avi = cv2.VideoWriter(avi_filename, fourcc, fps, (width, height))
+        for image in images:
+            avi.write(image)
         avi.release()
     print("Write {}".format(avi_filename))
 
