5bdf7057fe8982bcd80220966386512fb610e5f1
Roger Chen <rogerHH_chen@itri.org.tw>
2021-07-08 00:27:52 +0800

    fix crash

--- src/utilities/adv_to_server/src/Spat.py
+++ src/utilities/adv_to_server/src/Spat.py
@@ -7,9 +7,12 @@ s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 count = 0
 while 1:
     data = {"SPaT_MAP_Info":{"intersection_id": 0, "road_id": 1, "Latitude": 123.456,  "Longitude":456.789, "Spat_state" : 101, "Spat_sec": 15.5,  "Signal_state":0, "Index": count} }
+
+    data2 = "#1#2#3#4#"
     if not data:
         break
     s.sendto(json.dumps(data),addr)
+    #s.sendto(data2,addr)
     count += 1
     time.sleep(1) #delay 1 sec
 s.close()
