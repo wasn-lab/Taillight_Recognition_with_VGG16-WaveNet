f618a2cacf0626f1f73d741b3bb043bb89afae2b
Billhou <you@example.com>
2021-08-10 16:40:12 +0800

    add throttle in pkill and shutdown

--- src/scripts/bus_env_cbus/sudo-shutdown.sh
+++ src/scripts/bus_env_cbus/sudo-shutdown.sh
@@ -4,5 +4,6 @@ set -x
 ssh -t xavier "echo nvidia | sudo -S shutdown now"
 ssh -t local "echo itri | sudo -S shutdown now"
 ssh -t camera "echo itri | sudo -S shutdown now"
+ssh -t throttle "echo itri | sudo -S shutdown now"
 
 echo All done
