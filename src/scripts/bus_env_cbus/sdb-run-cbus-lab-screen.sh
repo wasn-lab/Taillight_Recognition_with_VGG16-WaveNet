2b21cc14445fedc69628d5c66f70f01e1db8473d
Billhou <you@example.com>
2021-08-10 13:22:58 +0800

    add c1 throttle.screen

--- src/scripts/bus_env_cbus/sdb-run-cbus-lab-screen.sh
+++ src/scripts/bus_env_cbus/sdb-run-cbus-lab-screen.sh
@@ -16,6 +16,7 @@ rosparam set /fail_safe/should_send_bags 0
 gnome-terminal -e "screen -c ${cur_dir}/lidar-lab.screen"
 gnome-terminal -e 'ssh -t local "screen -c /home/local/itriadv/src/scripts/bus_env_cbus/localization.screen"'
 gnome-terminal -e "ssh -t camera 'screen -c /home/camera/itriadv/src/scripts/bus_env_cbus/camera.screen'"
+gnome-terminal -e "ssh -t throttle 'screen -c /home/itri/itriadv/src/scripts/bus_env_cbus/throttle.screen'"
 
 sleep 30
 gnome-terminal -e 'ssh -t xavier "screen -c /home/nvidia/itriadv/src/scripts/bus_env_cbus/xavier.screen"'
