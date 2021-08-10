f2ec45bf9790c18ae0d6e81ca8ecbcc1562d6021
Billhou <you@example.com>
2021-08-10 13:33:13 +0800

    switch throttle screen to sdb-run-cbus-screen.sh

--- src/scripts/bus_env_cbus/sdb-run-cbus-screen.sh
+++ src/scripts/bus_env_cbus/sdb-run-cbus-screen.sh
@@ -19,6 +19,7 @@ rosparam set /car_model C1
 gnome-terminal -e "screen -c ${cur_dir}/lidar.screen"
 gnome-terminal -e 'ssh -t local "screen -c /home/local/itriadv/src/scripts/bus_env_cbus/localization.screen"'
 gnome-terminal -e "ssh -t camera 'screen -c /home/camera/itriadv/src/scripts/bus_env_cbus/camera.screen'"
+gnome-terminal -e "ssh -t throttle 'screen -c /home/itri/itriadv/src/scripts/bus_env_cbus/throttle.screen'"
 
 sleep 30
 gnome-terminal -e 'ssh -t xavier "screen -c /home/nvidia/itriadv/src/scripts/bus_env_cbus/xavier.screen"'
