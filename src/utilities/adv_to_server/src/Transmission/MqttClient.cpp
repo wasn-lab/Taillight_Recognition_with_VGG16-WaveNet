975e2a635c09a2d55ef9554a29a543c1e7fac29c
Roger Chen <rogerHH_chen@itri.org.tw>
2021-08-11 10:16:09 +0800

    Add mqtt C3 cert

--- src/utilities/adv_to_server/src/Transmission/MqttClient.cpp
+++ src/utilities/adv_to_server/src/Transmission/MqttClient.cpp
@@ -78,7 +78,10 @@ void MqttClient::setTLS()
     port = 3052;
   }else if(vid == "Kvu9zyAF5CAB"){
     car = "C2";
-    port = "3051"
+    port = "3051";
+  }else if(vid == "4nbbnxUszZGj"){
+    car = "C3";
+    port = 3051;
   }
   MQTT_CA_CRT = path + "/src/Transmission/TLS/" + car + "/ca.crt";
   MQTT_CLIENT_CRT = path + "/src/Transmission/TLS/" + car + "/client.crt";
