9a06a27fe54e0167b3bd82eb304bb3f84060abfd
amtommy6 <amtommy6@gmail.com>
2021-08-13 13:38:55 +0800

    Remove hostname

--- src/scripts/bus_env_cbus/camera/close_mps.sh
+++ src/scripts/bus_env_cbus/camera/close_mps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 if [ "$(pidof nvidia-cuda-mps-control)" ];then
-	gnome-terminal --tab -e  'bash -c "hostname; sudo_pw=itri; source $HOME/.bashrc;export CUDA_VISIBLE_DEVICES=0; echo $sudo_pw | sudo -S nvidia-smi -i 0 -c DEFAULT; echo $sudo_pw | echo quit | sudo nvidia-cuda-mps-control; exec bash"'
+	gnome-terminal --tab -e  'bash -c "sudo_pw=itri; source $HOME/.bashrc;export CUDA_VISIBLE_DEVICES=0; echo $sudo_pw | sudo -S nvidia-smi -i 0 -c DEFAULT; echo $sudo_pw | echo quit | sudo nvidia-cuda-mps-control; exec bash"'
   echo 'Close the mps.'
 else
   echo 'MPS is not found'
