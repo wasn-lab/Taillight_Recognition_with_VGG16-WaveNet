bad3098901c2f2da2cbebcb45ebd6c7b663725e5
amtommy6 <amtommy6@gmail.com>
2021-08-30 15:48:52 +0800

    Reverse mps

--- src/scripts/bus_env/camera/close_mps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-if [ "$(pidof nvidia-cuda-mps-control)" ];then
-	gnome-terminal --tab -e  'bash -c "sudo_pw=itri; source $HOME/.bashrc;export CUDA_VISIBLE_DEVICES=0; echo $sudo_pw | sudo -S nvidia-smi -i 0 -c DEFAULT; echo $sudo_pw | echo quit | sudo nvidia-cuda-mps-control; exec bash"'
-  echo 'Close the mps.'
-else
-  echo 'MPS is not found'
-fi
--- src/scripts/bus_env/camera/init_mps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-if [ "$(pidof nvidia-cuda-mps-control)" ];then
-  echo 'MPS was found'
-else
-   gnome-terminal --tab -e  'bash -c "sudo_pw=itri; source $HOME/.bashrc;export CUDA_VISIBLE_DEVICES=0; echo $sudo_pw | sudo -S nvidia-smi -i 0 -c EXCLUSIVE_PROCESS; echo sudo_pw | sudo -S nvidia-cuda-mps-control -f; exec bash"'
-  echo 'MPS is ready'
-fi
