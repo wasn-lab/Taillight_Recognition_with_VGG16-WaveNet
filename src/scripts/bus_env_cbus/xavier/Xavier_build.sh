2398fe03fc630721c3249e5fd5d48b3812b3923e
Billhou <you@example.com>
2021-09-01 16:35:19 +0800

    delete system_time from Xavier_build.sh

--- src/scripts/bus_env_cbus/xavier/Xavier_build.sh
+++ src/scripts/bus_env_cbus/xavier/Xavier_build.sh
@@ -10,5 +10,5 @@ if [[ -z "${car_model}" ]]; then
 fi
 
 pushd ${repo_dir}
-catkin_make -DCAR_MODEL=${car_model} -DCATKIN_WHITELIST_PACKAGES="camera_grabber;camera_utils;system_time;lidar_location_send;control_checker;gnss_utility;lidarxyz2lla;from_dspace;to_dspace;geofence;trimble_grabber;geofence_map_pp;geofence_map_pp_filter;vehinfo_pub;geofence_pp;rad_grab;control;car_model;dl_data;msgs;autoware_control_msgs;autoware_lanelet2_msgs;autoware_perception_msgs;autoware_planning_msgs;autoware_system_msgs;autoware_vehicle_msgs;image_compressor;dspace_tx;flag_management;lateral_control;long_control;speed_profile;geofence_switch;brake_can_io;steer_can_io;xbywire_checker;plc_fatek"
+catkin_make -DCAR_MODEL=${car_model} -DCATKIN_WHITELIST_PACKAGES="camera_grabber;camera_utils;lidar_location_send;control_checker;gnss_utility;lidarxyz2lla;from_dspace;to_dspace;geofence;trimble_grabber;geofence_map_pp;geofence_map_pp_filter;vehinfo_pub;geofence_pp;rad_grab;control;car_model;dl_data;msgs;autoware_control_msgs;autoware_lanelet2_msgs;autoware_perception_msgs;autoware_planning_msgs;autoware_system_msgs;autoware_vehicle_msgs;image_compressor;dspace_tx;flag_management;lateral_control;long_control;speed_profile;geofence_switch;brake_can_io;steer_can_io;xbywire_checker;plc_fatek"
 popd
