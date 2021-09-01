a5830858989ed7531cca1dec8058f216d619c67c
Billhou <you@example.com>
2021-09-01 09:58:58 +0800

    update init_throttle.sh with duty_cycle

--- src/scripts/bus_env_cbus/throttle/init_throttle.sh
+++ src/scripts/bus_env_cbus/throttle/init_throttle.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
-readonly car_model=$(rosparam get /car_model)
-if [[ "${car_model}" == "C2" ]]; then
-  source /home/throttle/itriadv/devel/setup.bash
-elif [[ "${car_model}" == "C3" ]]; then
-  source /home/throttle/itriadv/devel/setup.bash
-else
-  source /home/itri/itriadv/devel/setup.bash
-fi
 #source /home/itri/itriadv/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.7
 
-roslaunch daq_io daq_io.launch
+readonly car_model=$(rosparam get /car_model)
+if [[ "${car_model}" == "C2" ]]; then
+  source /home/throttle/itriadv/devel/setup.bash
+  roslaunch daq_io daq_io.launch duty_cycle_min:=40 duty_cycle_max:=75
+elif [[ "${car_model}" == "C3" ]]; then
+  source /home/throttle/itriadv/devel/setup.bash
+  roslaunch daq_io daq_io.launch duty_cycle_min:=40 duty_cycle_max:=75
+else
+  source /home/itri/itriadv/devel/setup.bash
+  roslaunch daq_io daq_io.launch duty_cycle_min:=65 duty_cycle_max:=85
+fi 
